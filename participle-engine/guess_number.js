9c3845a1c9e11e29415c1b4016db9cea6e394d0e
soumench <soumench@noreply.github.com>
2018-05-07 18:11:07 +0530

    tmod

--- participle-engine/guess_number.js
+++ participle-engine/guess_number.js
@@ -1,7 +1,7 @@
 function guessNumber(word){
                                 
-	let plurals = {mice:1,men:1,women:1,people:1,children:1,geese:1,teeth:1,oxen:1};
-	let AMBIGUOUS = {deer:1, moose:1, series:1, swine:1, salmon:1, means:1, aircraft:1, offspring:1, species:1, grapefruit:1,sheep:1,fish:1,shrimp:1,you:1};
+	let plurals = {mice:1,men:1,women:1,people:1,children:1,geese:1,teeth:1,oxen:1,you:1};
+	let AMBIGUOUS = {deer:1, moose:1, series:1, swine:1, salmon:1, means:1, aircraft:1, offspring:1, species:1, grapefruit:1,sheep:1,fish:1,shrimp:1};
 
 	if(/^(few|a few|the few)\s/.test(word) ) {return PLURAL;}
 
