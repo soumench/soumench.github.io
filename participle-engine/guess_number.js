92ebea9b318ab1de7b56ff24767a05dbfc6fd7df
soumench <soumench@noreply.github.com>
2018-05-17 23:36:42 +0530

    t mod

--- participle-engine/guess_number.js
+++ participle-engine/guess_number.js
@@ -4,9 +4,12 @@ function guessNumber(word){
                                 
 	let singulars = {geas:1,bias:1,virus:1,nucleus:1,iris:1,focus:1,gas:1,stress:1,bus:1,glass:1,mass:1,series:1,lens:1,ms:1,os:1,axis:1,basis:1,boss:1,species:1,brass:1,cross:1,necrosis:1,analysis:1,hypothesis:1,epidermis:1,locus:1,atlas:1,electrolysis:1,fungus:1,radius:1,arthritis:1,ethics:1,loss:1,meiosis:1,alias:1,genus:1,grass:1,gross:1,hepatitis:1,pancreas:1,witness:1,abscess:1,anus:1,census:1,compass:1,metastasis:1};
 	
-	let plurals = {mice:1,men:1,women:1,people:1,children:1,geese:1,teeth:1,oxen:1,you:1, "a few":1, them:1,fungi:1,loci:1,radii:1, all:1, both:1, many:1};
+	let plurals = {mice:1,men:1, women:1, people:1, children:1, geese:1, teeth:1, oxen:1, you:1, "a few":1, them:1, fungi:1, loci:1, radii:1, all:1, both:1, many:1, lives:1, wives:1, calves:1, leaves:1, knives:1,
+	lice:1, dice:1, pence:1, octopi:1, alumni:1, nuclei:1, crises:1, theses:1, analyses:1, axes:1, phenomena:1,
+	criteria:1, data:1, bacteria:1, strata:1, indices:1, apendices:1, vortices:1
+	};
 	
-	let AMBIGUOUS = {deer:1, moose:1, series:1, swine:1, salmon:1, means:1, aircraft:1, offspring:1, species:1, grapefruit:1,sheep:1,fish:1,shrimp:1};
+	let AMBIGUOUS = {deer:1, moose:1, series:1, swine:1, salmon:1, means:1, aircraft:1, offspring:1, species:1, grapefruit:1,sheep:1,fish:1,shrimp:1, buffalo:1, trout:1};
 	
 	if (word=="i")return SINGULAR;
 	 if(word.length < 2) return IDK;
