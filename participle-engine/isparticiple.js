5dc9d0f702e2e7d89a952329cb1e41e5cfdc6b8a
soumench <soumench@noreply.github.com>
2018-05-20 01:51:11 +0530

    s mod

--- participle-engine/isparticiple.js
+++ participle-engine/isparticiple.js
@@ -1,5 +1,6 @@
 let participle = {done:1, gone:1, taken:1, driven:1, shaken:1, woven:1, set:1, put:1, dug:1, cut:1,
-				 made:1, told:1, said:1, sought:1, bought:1, caught:1, brought:1, build:1};
+				 made:1, told:1, said:1, sought:1, bought:1, caught:1, brought:1, build:1, sworn:1,
+				 burnt:1};
 
 function isparticiple(word){
 	if(word.length<3){return 0;}
