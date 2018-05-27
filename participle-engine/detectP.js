303bdee438533a5ee88c131a79eec70ce5cf1171
soumench <soumench@noreply.github.com>
2018-05-28 01:26:01 +0530

    soumen mod

--- participle-engine/detectP.js
+++ participle-engine/detectP.js
@@ -1,6 +1,30 @@
 let lastauxiliary;
 let lastparticiple;
 
+ const goodauxiliaries={"": 1,has:2,have:2,had:3,"will have":4,"shall have":4, 
+                           "am":5, "is":5, "are":5, "was":6, "were":6};
+
+    const testmessages=["NOTHING DETECTED (by bad auxiliary)",
+         "just a participle",
+         "(has|have) + participle",
+         "had +  participle",
+         "(will|shall) + have + participle",
+         "(am|is|are) + participle",
+         "(was|were) + participle"         
+        ];
+        
+    const BARE_P=1;
+    const HASHAVE_P=2;
+    const HAD_P=3;
+    const WILLSHALLHAVE_P=4;
+    const AMISARE_P=5;
+    const WASWERE_P=6;
+
+
+
+
+
+
 function detectP(userinput)
     {
         let auxiliary="";    
