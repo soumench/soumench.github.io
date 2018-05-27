1a8380d6952ba2873c385610dcb483b06aa30be6
soumench <soumench@noreply.github.com>
2018-05-28 00:57:49 +0530

    soumen mod

---
+++ participle-engine/detectP.js
@@ -0,0 +1,28 @@
+function detectP(userinput)
+    {
+        let auxiliary="";    
+        let ispart=0;    
+        let lastword="";        
+        if(userinput){
+            auxiliary=userinput.replace(/\s*\S+$/,"");
+            lastword=userinput.replace(/^.*?\s*(\S+)$/,"$1");
+        }
+						
+        let auxnumber= goodauxiliaries[auxiliary];
+        if(!auxnumber)return 0; 
+        ispart= isparticiple(lastword);
+        if(!ispart)return 0;
+        lastparticiple=lastword;
+        lastauxiliary=auxiliary;
+       
+      /*  e("vardisplay").innerHTML=`
+${auxnumber?"good":"bad"}  auxiliary (${auxiliary}) <br/> 
+${ispart?"good":"bad"}  participle (${lastword}) <br/>         
+The auxiliary is: (${auxiliary}) <br/> 
+auxnumber: (${auxnumber}) <br/> 
+The lastword is : (${lastword}) <br/>
+ispart: (${ispart}) <br/>
+`; */
+        return auxnumber;
+    }
+    
