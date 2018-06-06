9c937fbfe062fab503bf5f1f5d82394b1a802917
soumench <soumench@noreply.github.com>
2018-06-06 21:40:27 +0530

    t s mod

--- participle-engine/nopassive.js
+++ participle-engine/nopassive.js
@@ -1,4 +1,4 @@
- let nopassive= {"is":1,"am":1,"are":1,"becomes":1,"became":1};
+ let nopassive= {"is":1,"am":1,"are":1,"becomes":1,"became":1, "become":1, "go":1, "goes":1, "went":1, "were":1};
 
     function hasnopassive(verb){
           
