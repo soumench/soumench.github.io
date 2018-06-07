d55da45cdfa2d722507199ba2e021d01aa98641f
soumench <soumench@noreply.github.com>
2018-06-07 23:13:58 +0530

    soumen mod

--- participle-engine/isparticiple.js
+++ participle-engine/isparticiple.js
@@ -11,7 +11,7 @@ let participle = {done:1, gone:1, taken:1, driven:1, shaken:1, woven:1, set:1, p
 				 slit:1, smelt:1, smelled:1, smitten:1, sowed:1, sown:1, spoken:1, sped:1, spelt:1, 
 				 spelled:1, spun:1, spat:1, spread:1, split:1, spoilt:1, spoiled:1, stood:1, stolen:1,
 				 stuck:1, stung:1, stridden:1, swum:1, taken:1, torn:1, thrown:1, worn:1, wed:1,
-				 wedded:1, won:1, wound:1, wrung:1, written:1};
+				 wedded:1, won:1, wound:1, wrung:1, written:1, hit:1};
 
 function isparticiple(word){
 	if(word.length<3){return 0;}
