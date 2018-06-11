826ef63750f7145e8d63ccc40f6a480713c97687
soumench <soumench@noreply.github.com>
2018-06-11 21:32:13 +0530

    Soumen mod

---
+++ ENGLISH/HS/XII/STRONG_ROOTS/questions.js
@@ -0,0 +1,24 @@
+const qa = [ 
+            1,"What is the meaning of 'erstwhile'? ", "Hint: former, old, past",            
+            2,"Where is Rameswaram located? ", "It is located in the erstwhile Madras State.",
+            3,"What was the father's name of Kalam? ", "Jainulabdeen",
+            4,"Who was Ashiamma?", "Kalam's mother.",
+            5,"Who were widely regarded as an ideal couple?", "Kalam's parents.",
+            6,"Where did Kalam live? ", "In his ancestral house, built in the middle of the 19th century.",
+            7,"How was the childhood of Kalam? ", "A very secure childhood, materially and emotionally.",
+			8,"Where did Kalam used to eat and with whom? ", "with mother, sitting on the floor of the kitchen. ",
+			9,"Describe the food that Kalam used to eat.","Rice, aromatic sambhar, a variety of pickles, coconut chutney",
+			10,"Which temple made Rameswaram a famous place?", "The Shiva temple",
+			11,"How far was the temple from Kalam's house? "," about a ten-minute walk",
+			12,"Where did Kalam's father take him in the evening and why? ", "to a very old mosque, for prayer",
+			13, "As a child, did Kalam understand the meaning of the Arabic prayers?","no",
+			14, "Who used to wait for Kalam's father after the evening prayer is over?","People of different religions",
+			15,"Whom did Kalam's father ask the people to thank?", " Allah, the merciful",
+			16,"Who was the high priest of Rameswaram Temple?", "pakshi Lakshmana Sastry",
+			17,"Who was able to convey complex spiritual concepts in very simple, down-to-earth Tamil?","Kalam's father, Jainulabdeen",
+			18,"When did Kalam's father use to start his day?", "at 4 am",
+			19,"How far was the coconut grove from Kalam's house?","about four miles away",
+			20,"Who revealed the fundamental truths to Kalam?","His father.",
+			21,"Did Kalam believe in the existence of a divine power?", "yes"
+        ];
+        const length = qa.length;
