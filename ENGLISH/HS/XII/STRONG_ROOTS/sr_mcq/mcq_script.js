2490915b12623c30b7fbf9b374e66f77440a85c3
soumench <soumench@noreply.github.com>
2018-06-13 01:37:12 +0530

    soumen mod

--- ENGLISH/HS/XII/STRONG_ROOTS/sr_mcq/mcq_script.js
+++ ENGLISH/HS/XII/STRONG_ROOTS/sr_mcq/mcq_script.js
@@ -32,20 +32,128 @@
 			{
 			  question: "Who bestowed the title of \"Bahadur\" upon one of Ashiamma's forebears?",
 			  answers: {
-				a: "The French",
-				b: "The British",
-				c: "The German",
+				a: "The French.",
+				b: "The British.",
+				c: "The German.",
 			 },
 			  correctAnswer: "b"
 			},
 			{
 			  question: "Kalam was ",
 			  answers: {
-				a: "a short boy with rather undistinguished look",
-				b: "a handsome boy",
-				c: "a pretty boy",
+				a: "a short boy with rather undistinguished look.",
+				b: "a handsome boy.",
+				c: "a pretty boy.",
 			 },
 			  correctAnswer: "a"
+			},
+			{
+			  question: "The childhood of Kalam was ",
+			  answers: {
+				a: "insecure.",
+				b: "emotionally insecure.",
+				c: "very secure.",
+			 },
+			  correctAnswer: "c"
+			},
+			{
+			  question: "The locality  of Kalam was ",
+			  answers: {
+				a: "full of Hindus.",
+				b: "full of Christians.",
+				c: "predominantly Muslim.",
+			 },
+			  correctAnswer: "c"
+			},
+			{
+			  question: "Many people of different religions offered Kalam's father ",
+			  answers: {
+				a: "a bowl of soup",
+				b: "a bowl of water",
+				c: "a bowl of curry",
+			 },
+			  correctAnswer: "b"
+			},
+			{
+			  question: "Many people of different religions visited Kalam's father ",
+			  answers: {
+				a: "to thank him.",
+				b: "to scold him.",
+				c: "to preach him.",
+			 },
+			  correctAnswer: "a"
+			},
+			{
+			  question: "Pakshi Lakshmana Sastry was the ",
+			  answers: {
+				a: "saint of the temple.",
+				b: "the high priest of the temple.",
+				c: "the low priest of the temple.",
+			 },
+			  correctAnswer: "b"
+			},
+			{
+			  question: "Prayer made possible ",
+			  answers: {
+				a: "a difference of spirit between people.",
+				b: "a difference of opinion between people.",
+				c: " a communion of the spirit between people.",
+			 },
+			  correctAnswer: "c"
+			},
+			{
+			  question: "Adversity always presents opportunities for ",
+			  answers: {
+				a: "inspection.",
+				b: "interception.",
+				c: "introspection.",
+			 },
+			  correctAnswer: "c"
+			},
+			{
+			  question: "impasse stands for",
+			  answers: {
+				a: "a situation in which no progress is possible.",
+				b: "a situation  which is amicable.",
+				c: "a situation which is advantageous.",
+			 },
+			  correctAnswer: "a"
+			},
+			{
+			  question: "Kalam's father used to start his day",
+			  answers: {
+				a: "at six am",
+				b: "at four am.",
+				c: "at seven am.",
+			 },
+			  correctAnswer: "b"
+			},
+			{
+			  question: "Kalam tried to emulate his his father ",
+			  answers: {
+				a: "in his own world of religion.",
+				b: "in his own world of sociology.",
+				c: "in his own world of science and technology.",
+			 },
+			  correctAnswer: "c"
+			},
+			{
+			  question: "Kalam firmly believed that ",
+			  answers: {
+				a: "there exists a divine power.",
+				b: "there exists a mechanical power.",
+				c: "there exists a logical power.",
+			 },
+			  correctAnswer: "a"
+			},
+			{
+			  question: " A person is on the road to freedom, happiness and peace of mind ",
+			  answers: {
+				a: "once an individual studies well.",
+				b: "once an individual severs his emotional and physical bond.",
+				c: "once an individual does physical exercise.",
+			 },
+			  correctAnswer: "b"
 			}
 		  ];
 
