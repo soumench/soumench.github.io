94cae86d8e355ee273461779a3c9a962ffe1750e
soumench <soumench@noreply.github.com>
2018-06-20 21:05:58 +0530

    soumen mod

--- Javascript-lessons/TRUE-FALSE QUESTIONS TEMPLATE/js/tfq.js
+++ Javascript-lessons/TRUE-FALSE QUESTIONS TEMPLATE/js/tfq.js
@@ -1,6 +1,14 @@
  const q=[
-			"Any name is noun.", true, 
-			"A verb describes a thing.", false,
-			"Pronoun stands in place of noun.", true,
-			"How things are done, the adjectives tell.", false
+			"Boats sail on the rivers.", true, 
+			"Ships sail across the sky.", false,
+			"Clouds are prettier than boats and ships.", true,
+			"There are bridges on the seas.", false,
+	 		"The rainbow is not beautiful.", false,
+	 		"The rainbow builds a road from the earth to sky.", true,
+	 		"A river is bigger than a sea.", false,
+	 		"A ship is bigger than a boat.", true,
+	 		"The poet praises the beauty of the rainbow.",true,
+	 		"The rainbow builds a real road from the earth to the sky.",false,
+	 		"The shape of the rainbow is circular.", true,
+	 		"A rainbow is formed by the sunlight and water droplets.", true
 		];
