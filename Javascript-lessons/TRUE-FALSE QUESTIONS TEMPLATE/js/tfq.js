1777e36a05efba41d8ce2b3557c58c16262001df
soumench <soumench@noreply.github.com>
2018-06-20 19:55:06 +0530

    soumen mod

---
+++ Javascript-lessons/TRUE-FALSE QUESTIONS TEMPLATE/js/tfq.js
@@ -0,0 +1,6 @@
+ const q=[
+			"Any name is noun.", true, 
+			"A verb describes a thing.", false,
+			"Pronoun stands in place of noun.", true,
+			"How things are done, the adjectives tell.", false
+		];
