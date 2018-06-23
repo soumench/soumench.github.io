850f1414ecdf7ae3070b61ddaf223584dcfb6088
soumench <soumench@noreply.github.com>
2018-06-23 18:01:18 +0530

    Soumen Mod

---
+++ ENGLISH/SE/V/SVO_CONSTRUCTION_HELPER/js/mass_move.js
@@ -0,0 +1,28 @@
+/*move_subjects();*/
+function move_subjects(){
+	move("soumen", r1, col1);
+	move("i", r2, col1);        
+	move("we", r3, col1);
+	move("you", r4, col1);
+	move("he", r5, col1);
+	move("they", r6, col1);
+}
+
+/*move_verbs();*/
+function move_verbs(){
+	move("plays", r1, col2);
+	move("love", r2, col2);         
+	move("buy", r3, col2);
+	move("wrote", r4, col2);
+	move("eating", r5, col2);
+	move("made", r6, col2);
+}
+/*move_objects();*/
+function move_objects(){
+	move("chess", r1, col3);
+	move("icecream", r2, col3);        
+	move("computer", r3, col3);
+	move("poem", r4, col3);
+	move("biryani", r5, col3);
+	move("toy", r6, col3);
+}
