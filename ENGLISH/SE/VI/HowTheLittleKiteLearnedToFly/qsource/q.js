1050be68215840107fd4dcf22ea4d7fb6bb215f6
soumench <soumench@noreply.github.com>
2018-06-25 22:00:08 +0530

    soumen mod

---
+++ ENGLISH/SE/VI/HowTheLittleKiteLearnedToFly/qsource/q.js
@@ -0,0 +1,98 @@
+const topic_name =" M.C.Q.-\"How The Little Kite Learned to Fly\""; 
+const list=[
+            "Who wrote this poem?",
+	 		"Wordsworth.",
+	 		"Katherine Pyle.",
+	 		"Shakespeare.",
+	 		"b",
+	 
+	 
+            "The little kite at first said..",
+	 		"\"I can fly for sure\".",
+	 		"\"I sure can do it\".",
+	 		"\"I never can do it\".",
+	 		"c",
+	
+	        "The little kite was",
+	 		"sorry to fly.",
+	 		"afraid to fly.",
+	 		"happy to fly.",
+	 		"b",
+	 		
+	 		"The little kite was afraid of ",
+	 		"dying.",
+	 		"falling.",
+	 		"being alone.",
+	 		"b",
+	 		
+	 		"The big kite rose towards the",
+	 		"tranquil sky.",
+	 		"stormy sky.",
+	 		"space.",
+	 		"a",
+	 		
+	 		"The little kite shook himself free",
+	 		"for flight.",
+	 		"for fight.",
+	 		"for tight.",
+	 		"a",
+	 		
+	 		"The little kite was  ",
+	 		"falling steadily.",
+	 		"rising steadily.",
+	 		"fumbling steadily.",
+	 		"b",
+	 
+	 		"What is the meaning of whirling?",
+	 		"whirlpool.",
+	 		"wheel chair.",
+	 		"moving rapidly round and round.",
+	 		"c",
+	 
+	 		"The little kite was  ",
+	 		"thrilled with pride.",
+	 		"sorry.",
+	 		"morose.",
+	 		"a",
+	 
+	 		"The little kite was flying ",
+	 		"all alone.",
+	 		"with the big kite side by side.",
+	 		"with an airplane.",
+	 		"b",
+	 
+	 		"Both the kites rested ",
+	 		"on the mountain.",
+	 		"on tree-top.",
+	 		"high up in the quiet air.",
+	 		"c",
+	 
+	 		"The boys on the ground below appeared like ",
+	 		"small spots.",
+	 		"small pox.",
+	 		"small box.",
+	 		"a",
+	 
+	 		"The little kite became successful because",
+	 		"big kite pushed him.",
+	 		"he tried bravely.",
+	 		"he was never afraid.",
+	 		"b",
+	 
+	 		"Birds and clouds accompanied the kites",
+	 		"high up on the mountain.",
+	 		"high up on the tree-top.",
+	 		"high up in the sky.",
+	 		"c",
+	 
+	 		"What does the poem teach you?",
+	 		"We can be easily successful.",
+	 		"We must try and try again to become successful.",
+	 		"We can never be successful.",
+	 		"b"         
+			
+	
+        ];
+
+		/*&copy Made by Soumen Chakraborty - 2018 <br>
+    	Strictly for Educational and Non-commercial purpose only.*/
