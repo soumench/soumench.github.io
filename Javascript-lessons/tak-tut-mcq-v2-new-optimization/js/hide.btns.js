602b4bbb63f6505852bd41020de7368219e31ca2
soumench <soumench@noreply.github.com>
2018-06-26 00:32:21 +0530

    soumen mod

--- Javascript-lessons/tak-tut-mcq-v2-new-optimization/js/hide.btns.js
+++ Javascript-lessons/tak-tut-mcq-v2-new-optimization/js/hide.btns.js
@@ -1,7 +1,7 @@
 function hide_buttons(){
 	/*e("abtn").style.visibility=e("bbtn").style.visibility=e("cbtn").style.visibility="hidden";*/
 	
-	e("a").style.color=e("b").style.color=e("c").style.color="white";
+	/*e("a").style.color=e("b").style.color=e("c").style.color="white";*/
 	
 	/*e("next").style.visibility=e("prev").style.visibility ="visible";*/
 }       
