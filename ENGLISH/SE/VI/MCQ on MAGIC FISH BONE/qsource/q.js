0a3aae98938e8b23e94577df828b00fb3a24e751
soumench <soumench@noreply.github.com>
2018-06-26 20:38:44 +0530

    soumen mod

---
+++ ENGLISH/SE/VI/MCQ on MAGIC FISH BONE/qsource/q.js
@@ -0,0 +1,136 @@
+const topic_name =" M.C.Q. on \"The Magic Fish Bone\""; 
+const list=[
+            "Who wrote the popular short story \"The Magic Fish bone\"?",
+	 		"Willim Wordsworth.",
+	 		"Rabindranath Tagore.",
+	 		"Charles Dickens, a very famous British novelist.",
+	 		"c",
+	 
+	 
+            "Who had a queen and several children?",
+	 		"The Fairy.",
+	 		"The Minister.",
+	 		"The King.",
+	 		"c",
+	
+	        "Who is a fish monger?",
+	 		"a goat seller.",
+	 		"a fish seller.",
+	 		"a fish eater.",
+	 		"b",
+	 		
+	 		"What fish did the King buy? ",
+	 		"Salman.",
+	 		"Salmon.",
+	 		"Samon.",
+	 		"b",
+	 		
+	 		"The word errand-boy means-",
+	 		"a boy who works in a shop.",
+	 		"an erratic boy.",
+	 		"a boy who errs a lot.",
+	 		"a",
+	 		
+	 		"The old lady was invisible to the King at first.",
+	 		"True.",
+	 		"False.",
+	 		"There was no old lady .",
+	 		"a",
+	 		
+	 		"The quantity of fish the King bought was",
+	 		"1.5kg.",
+	 		"1.5 pound.",
+	 		"1.5 ounce.",
+	 		"b",
+	 
+	 		"The old lady was a ",
+	 		"fiery.",
+	 		"Wayfarer.",
+	 		"fairy.",
+	 		"c",
+	 
+	 		"The name of the King was ",
+	 		"King Watkins the first.",
+	 		"King Oat King.",
+	 		"King Kong.",
+	 		"a",
+	 
+	 		"The name of the King's eldest daughter was",
+	 		"Alisha.",
+	 		"Alicia.",
+	 		"All Asia.",
+	 		"b",
+	 
+	 		"The King met the fairy and went to his ",
+	 		"palace.",
+	 		"garden.",
+	 		"office.",
+	 		"c",
+	 
+	 		"The King was ",
+	 		"extremely poor.",
+	 		"extremely rich.",
+	 		"extremely glad.",
+	 		"a",
+	 
+	 		"The King had ",
+	 		"no queen.",
+	 		"no money.",
+	 		"no bunny.",
+	 		"b",
+	 
+	 		"Alicia kept the magic fish-bone ",
+	 		"in a secret place.",
+	 		"in her almirah.",
+	 		"in her pocket.",
+	 		"c",
+	 
+	 		"The magic fish-bone can be used ",
+	 		"twice.",
+	 		"only once.",
+	 		"innumerable times.",
+	 		"b",
+			
+			"Alicia kissed the bone and wished",
+	 		"it was a holiday.",
+	 		"it was salary day.",
+	 		"it was a May day.",
+	 		"b",  
+			
+			"The old fairy was",
+	 		"charming.",
+	 		"charminar.",
+	 		"charred.",
+	 		"a",
+	
+			"The King's salary came ",
+	 		"rattling down the roof.",
+	 		"rattling down the wall.",
+	 		"rattling down the chimney.",
+	 		"c",
+	
+			"The magic fish-bone shone like a ",
+	 		"father-of-pearl.",
+	 		"mother-of-pearl.",
+	 		"brother-of-pearl.",
+	 		"b",
+	
+			"At last the magic fish-bone was grabbed by ",
+	 		"the pet cat.",
+	 		"the pet pug-dog  .",
+	 		"the squirrel.",
+	 		"b",
+		
+			"Alicia was ",
+	 		"very beautiful.",
+	 		"very hostile.",
+	 		"very ugly.",
+	 		"a", 
+			
+			"The King was a ",
+	 		"a very lazy man.",
+	 		"a very dishonest man.",
+	 		"a very honest and hardworking man.",
+	 		"c" 
+	
+        ];
