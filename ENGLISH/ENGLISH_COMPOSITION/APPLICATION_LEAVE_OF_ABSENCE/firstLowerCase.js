248ad2f83177985d193aa81b2df39a35c649a6ab
soumench <soumench@noreply.github.com>
2018-07-05 19:38:29 +0530

    soumen mod

---
+++ ENGLISH/ENGLISH_COMPOSITION/APPLICATION_LEAVE_OF_ABSENCE/firstLowerCase.js
@@ -0,0 +1,3 @@
+function firstLowerCase(txt) {
+            return txt.substr(0, 1).toLowerCase() + txt.substr(1);
+        }
---
+++ ENGLISH/ENGLISH_COMPOSITION/APPLICATION_LEAVE_OF_ABSENCE/firstUpperCase.js
@@ -0,0 +1,3 @@
+function firstUpperCase(txt) {
+            return txt.substr(0, 1).toUpperCase() + txt.substr(1);
+        }
