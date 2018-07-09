ee5b38f845b980923dc5aa0fbe84d9c5095af8ef
soumench <soumench@noreply.github.com>
2018-07-09 21:11:58 +0530

    soumen mod

---
+++ ENGLISH/SE/V/MCQ on Beyond Barriers/qsource/q.js
@@ -0,0 +1,112 @@
+const topic_name =" M.C.Q. on \"Beyond Barriers\""; 
+const list=[
+            "Who was first Indian Space Traveller?",
+	 		"Rakesh Khanna.",
+	 		"Rakesh Roshan.",
+	 		"Rakesh Sharma.",
+	 		"c",
+	 
+	 
+            "When was Rakesh born?",
+	 		"in 1950.",
+	 		"in 1947.",
+	 		"in 1949.",
+	 		"c",
+	
+	        "Rakesh had his schooling in Hyderabad.",
+	 		"False.",
+	 		"True.",
+	 		"I don't know.",
+	 		"b",
+	 		
+	 		"Rakesh went to space on",
+	 		"2nd April,1984.",
+	 		"3rd April,1984.",
+	 		"4th April,1984.",
+	 		"b",
+	 		
+	 		"Rakesh went to space on ",
+	 		"Soyuz T-11",
+	 		"Suraj T-11.",
+	 		"Swaraj T-11.",
+	 		"a",
+	 		
+	 		"How long did Rakesh spend in space? ",
+	 		"Seven days, twenty one hours and forty minutes.",
+	 		"Seventeen days, twenty one hours and forty minutes.",
+	 		"Six days, twenty one hours and forty minutes.",
+	 		"a",
+	 		
+	 		"When in Space, Rakesh talked to",
+	 		"Mr. Rajib Gandhi.",
+	 		"Mrs. Indira Gandhi.",
+	 		"Mahatma Gandhi.",
+	 		"b",
+	 
+	 		"What was Rakesh's reply to the Prime Minister's question?",
+	 		"Vande Mataram.",
+	 		"He was silent.",
+	 		"Sare Jahan Se Achcha.",
+	 		"c",
+	 
+	 		"When was Kalpana Chawla born? ",
+	 		"in 1962.",
+	 		"in 1972.",
+	 		"in 1982.",
+	 		"a",
+	 
+	 		"In November 1996, Kalpana joined",
+	 		"mission TSS-87.",
+	 		"mission STS-87.",
+	 		"mission STS-89.",
+	 		"b",
+	 
+	 		"From Space, Kalpana spoke with  the then prime minister ",
+	 		"Mrs. Indira Gandhi.",
+	 		"Mr. Rajib Gandhi.",
+	 		"Mr. I.K. Gujral.",
+	 		"c",
+	 
+	 		"On her last mission, Kalpana was on board",
+	 		"the space shuttle Columbia.",
+	 		"the space shuttle Soyuz T-11.",
+	 		"moon rover.",
+	 		"a",
+	 
+	 		"When did Kalpana die?",
+	 		"on 21st February, 2003.",
+	 		"on 1st February, 2003.",
+	 		"on 1st March, 2003.",
+	 		"b",
+	 
+	 		"The Columbia space shuttle exploded with all its crew ",
+	 		"during its launch.",
+	 		"when in space.",
+	 		"during re-entry into earth's atmosphere.",
+	 		"c",
+	 
+	 		"Kalpana was a",
+	 		"a pilot.",
+	 		"research scientist at NASA.",
+	 		"a captain.",
+	 		"b",
+			
+			"Kalpana studied  ",
+	 		"Computer Engineering.",
+	 		"Aeronautical Engineering.",
+	 		"Political Science.",
+	 		"b",  
+			
+			"Kalpana went to the USA to study   ",
+	 		"at Yale University.",
+	 		"at the University of Texas.",
+	 		"at Columbia University.",
+	 		"b",
+	
+			"Kalpana's story reminds us of   ",
+	 		"her wealth.",
+	 		"the value of hard work and sincerity.",
+	 		"her gregarious nature.",
+	 		"b" 
+			
+        ];
