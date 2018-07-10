6c09759aade742dd621502dca25bb2012b4efa0a
soumench <soumench@noreply.github.com>
2018-07-10 20:22:21 +0530

    soumen mod

---
+++ ENGLISH/SE/VIII/MCQ on THE MAN WHO PLANTED TREES/qsource/q.js
@@ -0,0 +1,142 @@
+const topic_name =" M.C.Q. on \"The Man Who Planted Trees\""; 
+const list=[
+            "Jean Giono was a ",
+	 		"American author.",
+	 		"English author.",
+	 		"French author.",
+	 		"c",
+	 
+	 
+            "The author went on hike",
+	 		"some 50 years ago.",
+	 		"some 14 years ago.",
+	 		"some 40 years ago.",
+	 		"c",
+	
+	        "The author was travelling in the .",
+	 		"Himalayan region.",
+	 		"Alps region.",
+	 		"an unknown mountain region.",
+	 		"b",
+	 		
+	 		"The author walked for ",
+	 		"two days.",
+	 		"three days.",
+	 		"five days.",
+	 		"b",
+	 		
+	 		"The author came to ",
+	 		"an abandoned village.",
+	 		"an abandoned city.",
+	 		"an abandoned town.",
+	 		"a",
+	 		
+	 		"The spring was",
+	 		"completely dry.",
+	 		"full of water.",
+	 		"non-existent.",
+	 		"a",
+	 		
+	 		"In June the day was",
+	 		"hectic.",
+	 		"beautiful.",
+	 		"very cold.",
+	 		"b",
+	 
+	 		"The author was desperately in search of ",
+	 		"a bird.",
+	 		"an animal.",
+	 		"water.",
+	 		"c",
+	 
+	 		"Finally the author came across ",
+	 		"a shepherd.",
+	 		"a cowboy.",
+	 		"a vendor.",
+	 		"a",
+	 
+	 		"The shepherd led the author to",
+	 		"an abandoned village.",
+	 		"his cottage.",
+	 		"a spring.",
+	 		"b",
+	 
+	 		"The shepherd offered the author his",
+	 		"coffee.",
+	 		"tea.",
+	 		"soup.",
+	 		"c",
+	 
+	 		"The company of the man brought the author",
+	 		"a feeling of peace.",
+	 		"a feeling of agony.",
+	 		"a feeling of hostility.",
+	 		"a",
+	 
+	 		"Acorns are",
+	 		"seeds of corns.",
+	 		"fruits of the oak tress.",
+	 		"small unicorns.",
+	 		"b",
+	 
+	 		"The man was planting ",
+	 		"mango trees.",
+	 		"orange trees.",
+	 		"oak trees.",
+	 		"c",
+	 
+	 		"The man had been planing trees for",
+	 		"two years.",
+	 		"three years.",
+	 		"ten years.",
+	 		"b",
+			
+			"The name of the shepherd was",
+	 		"Alexander.",
+	 		"Elzeard Bouffier.",
+	 		"Elton john.",
+	 		"b",  
+			
+			"The man lives",
+	 		"with his family.",
+	 		"with his flocks of sheep and his dog.",
+	 		"with his wife and son.",
+	 		"b",
+	
+			"The author was engaged in war",
+	 		"for two years.",
+	 		"for five years.",
+	 		"for three years.",
+	 		"b", 
+	
+			"The man concluded that his country was dying for",
+	 		"lack of trees.",
+	 		"lack of people.",
+	 		"lack of water.",
+	 		"a",
+	
+			"How long was the forest?",
+	 		"ten kilometers.",
+	 		"twelve kilometers.",
+	 		"eleven kilometers.",
+	 		"c",
+	
+			"The forest scene was",
+	 		"impressive.",
+	 		"ugly.",
+	 		"deadly.",
+	 		"a", 
+	
+			"The war had not disturbed the shepherd at all.",
+	 		"True.",
+	 		"False.",
+	 		"No idea.",
+	 		"a",
+	
+			"The man could be as effective as God",
+	 		"in destruction.",
+	 		"in domains other than destruction.",
+	 		"in killing trees.",
+	 		"b"
+			
+        ];
