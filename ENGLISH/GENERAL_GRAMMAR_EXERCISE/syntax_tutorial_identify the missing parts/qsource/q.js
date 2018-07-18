0c0636b86c2aa8e9585b1a6f623843177449d33d
Soumen <soumen@users.noreply.github.com>
2018-07-18 21:32:04 +0530

    soumen mod

---
+++ ENGLISH/GENERAL_GRAMMAR_EXERCISE/syntax_tutorial_identify the missing parts/qsource/q.js
@@ -0,0 +1,149 @@
+const topic_name =" Select the Correct Missing part"; 
+const list=[
+            "I ___ a student.",
+	 		"are",
+	 		"is",
+	 		"am",
+	 		"c",
+	 
+	 
+            "___ I come in, sir?",
+	 		"My",
+	 		"Mai",
+	 		"May",
+	 		"c",
+	
+	        "English ___ my favourite subject.",
+	 		"are",
+	 		"is",
+	 		"were",
+	 		"b",
+	 		
+	 		"I ___ at a small village.",
+	 		"love",
+	 		"live",
+	 		"liv",
+	 		"b",
+	 		
+	 		"___ did it happen?",
+	 		"When",
+	 		"what",
+	 		"who",
+	 		"a",
+	 		
+	 		"___ are you?",
+	 		"How",
+	 		"when",
+	 		"which",
+	 		"a",
+	 		
+	 		"___ are you?",
+	 		"where",
+	 		"Where",
+	 		"Wher",
+	 		"b",
+	 
+	 		"I ___ not expected that.",
+	 		"am",
+	 		"has",
+	 		"have",
+	 		"c",
+	 
+	 		"The teacher ___ me.",
+	 		"advised",
+	 		"adviced",
+	 		"adhesive",
+	 		"a",
+	 
+	 		"I want to go ___.",
+	 		"to shopping",
+	 		"shopping",
+	 		"shoping",
+	 		"b",
+	 
+	 		"I ___ Biryani yesterday.",
+	 		"eat",
+	 		"eaten",
+	 		"ate",
+	 		"c",
+	 
+	 		"He ___ singing a song.",
+	 		"is.",
+	 		"has",
+	 		"are",
+	 		"a",
+	 
+	 		"___ has done the homework.",
+	 		"We",
+	 		"She",
+	 		"They",
+	 		"b",
+	 
+	 		"I ___ an icecream.",
+	 		"wont",
+	 		"went",
+	 		"want",
+	 		"c",
+	 
+	 		"Ravi ___ my best friend.",
+	 		"are",
+	 		"is",
+	 		"has",
+	 		"b",
+			
+			"I believe in the ___ of prayer.",
+	 		"powder",
+	 		"power",
+	 		"pour",
+	 		"b",  
+			
+			"This is ___ Indian army.",
+	 		"an",
+	 		"a",
+	 		"and",
+	 		"a",
+	
+			"Coal is ___.",
+	 		"blake",
+	 		"block",
+	 		"black",
+	 		"c",
+	
+			"Kindness is a ___ virtue.",
+	 		"grate",
+	 		"great",
+	 		"gate",
+	 		"b",
+	
+			"The ___ is smiling.",
+	 		"grill",
+	 		"girl",
+	 		"grail",
+	 		"b",
+		
+			"The ___ is a domestic animal.",
+	 		"dog",
+	 		"giraffe",
+	 		"elephant",
+	 		"a", 
+			
+			"How ___ is the pen?",
+	 		"mach",
+	 		"munch",
+	 		"much",
+	 		"c",
+			
+			"Can you ___ your name?",
+	 		"spoil",
+	 		"spill",
+	 		"spell",
+	 		"c",
+	
+			"Good ___.",
+	 		"bi",
+	 		"bye",
+	 		"by",
+	 		"b"
+	
+			
+        ];
