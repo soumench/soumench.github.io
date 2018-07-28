2158b150a946d18046f75644e5fe9638ffc85376
Soumen <soumen@users.noreply.github.com>
2018-07-28 18:03:54 +0530

    soumen mod

---
+++ ENGLISH/GENERAL_GRAMMAR_EXERCISE/prefix-suffix-game/js/ps.js
@@ -0,0 +1,79 @@
+const list = [
+    "un","unhappy","prefix",
+            
+    "less","tasteless","suffix",
+            
+    "able","presentable", "suffix",
+    
+    "im", "impatient", "prefix",
+    
+    "a", "aside", "prefix",
+    
+    "ing", "making", "suffix",
+    
+    "co", "co-worker", "prefix",
+    
+    "ous", "religious", "suffix",
+    
+    "ex", "ex-president", "prefix",
+    
+    "micro", "microscope", "prefix",
+    
+    "ism", "escapism", "suffix",
+    
+    "mis", "misunderstand", "prefix",
+    
+    "re", "rediscover", "prefix",
+    
+    "sub", "sub-category", "prefix",
+    
+    "tri", "tripod", "prefix",
+    
+    "wise", "clockwise", "suffix",
+    
+    "uni", "universal", "prefix",
+    
+    "al", "denial", "suffix",
+    
+    "super", "superstar", "prefix", 
+    
+    "dom", "freedom", "suffix",
+    
+    "omni", "omnipotent", "prefix",
+    
+    "ment", "argument", "suffix",
+    
+    "homo", "homogeneous", "prefix", 
+    
+    "ness", "sickness", "suffix",
+    
+    "post", "postpone", "prefix",
+    
+    "ity", "validity", "suffix",
+    
+    "para", "paradox", "prefix",
+    
+    "tion", "promotion", "suffix",
+    
+    "mid", "midfielder", "prefix", 
+    
+    "ful", "hopeful", "suffix",
+    
+    "ish", "childish", "suffix",
+    
+    "ly", "badly", "suffix",
+            
+    "cal", "musical", "suffix",
+    
+    "semi", "semi-final", "prefix",
+        
+    "ive", "attentive", "suffix",
+        
+    "mono", "monolithic", "prefix",
+    
+    "trans", "transport", "prefix", 
+        
+    "ship", "friendship", "suffix",
+        
+    "anti", "antibiotic", "prefix"            
+];
---
+++ ENGLISH/GENERAL_GRAMMAR_EXERCISE/prefix-suffix-game/js/rot.js
@@ -0,0 +1,3 @@
+function rotate(id, degree){
+    e(id).style = 'transform: rotate(' + degree + 'deg)';
+}
