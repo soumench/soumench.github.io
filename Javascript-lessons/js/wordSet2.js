8804b538966f19e93f5ce38f85af788cf6c360ba
Soumen <soumen@users.noreply.github.com>
2018-09-10 01:06:58 +0530

    soumen

---
+++ Javascript-lessons/js/wordSet2.js
@@ -0,0 +1,15 @@
+//set 2
+
+var englishWords = [
+    "their", "kill", "who", "but", "sleep"
+    , "then", "first", "heart", "so", "name"
+    , "sit", "animal", "sharp", "more", "look"
+    , "moon", "because", "new", "us", "path"
+
+];
+var bengaliWords = [
+    "tAder", "hatyA karA", "ke", "kintu", "ghumAnO"
+    , "takhan", "pratham", "hRday", "sutarAG", "nAm"
+    , "basA", "jantu", "dhArAlO", "Aro", "tAkAnO"
+    , "cAnd", "kAran", "natun", "AmAderke", "rAstA/path"
+];
