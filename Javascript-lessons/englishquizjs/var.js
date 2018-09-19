4712243271b95442182894e6914d0a267ce78738
Soumen <soumen@users.noreply.github.com>
2018-09-20 02:03:49 +0530

    soumen

--- Javascript-lessons/englishquizjs/var.js
+++ Javascript-lessons/englishquizjs/var.js
@@ -1,3 +1,5 @@
+let isBlink = false;
+let timeoutCounter=0; 
 var b0;
 var b1;
 var b2;
@@ -5,4 +7,39 @@ var b3;
 
 
 
-function init() {   b0=e("b0");  b1=e("b1"); b2=e("b2"); b3=e("b3"); }
+const waitclick_timeout=15000;
+const delayafterrightanswer = 1500;
+const delayafterwronganswer=5000;
+
+var i=0;
+var rightbutton = 0;
+var good_chosen=false;
+var idk_chosen=true;
+var wrongbutton;
+var cd;
+var countdown_timer;
+var nextq;
+
+var rightanswersc;
+var wronganswersc;
+var idkanswersc;
+var totalanswers;
+
+const WRONGS=3;
+var BUTTONS=WRONGS+1;
+
+
+const success = new Audio("audio/success.oga");
+const failure = new Audio("audio/fail.oga");
+const noIdea = new Audio("audio/idk.oga");
+
+
+
+
+stopquiz.style.visibility="hidden";
+
+function init() {   b0=e("b0");  b1=e("b1"); b2=e("b2"); b3=e("b3"); 
+                    
+                 
+                
+                }
