adcb461b485b4c2d24998cf67d3156e10c1f1a41
Soumen <soumen@users.noreply.github.com>
2018-10-07 13:13:36 +0530

    copyright-soumen

---
+++ ENGLISH/HS/XII/XII-assorted-grammar-2018/questions.js
@@ -0,0 +1,60 @@
+const qa = [ 
+            1,"Shakespeare was a great playwright. <br>He wrote Macbeth.<br>( Join into a complex sentence.)", "Shakespeare was a great playwright who wrote Macbeth.", 
+    
+            2,"I knew him.<br>(change the voice)", "He was known to me.",
+    
+            3,"What a lovely sight! <br>( Change into an assertive sentence) ", "It is a very lovely sight.",
+        
+            4,"I am quite sure of her success.<br>(Use \"succeed\" and rewrite.)", "I am quite sure that she will succeed.",
+    
+            5,"The teacher said to the students, \"The earth moves round the sun.\" <br>(Change into Indirect Speech.)", "The teacher told the students that the earth moves round the sun.",
+        
+            6,"The sun rises in the east.<br> We all know it.<br> (Use noun clause.)", "We all know that the sun rises in the east.",
+        
+            7,"My cat ate a rat. <br>She went away.<br> (Use nominative absolute.)", "Having eaten a rat, my cat went away.",
+    
+    
+			8,"The old man in our neighbourhood is hard-working. <br> He is honest.<br>(Use a cumulative conjunction.)", "The old man in our neighbourhood is both hard-working and honest.",
+    
+    
+			9,"I still have a little money. <br> I can buy some food for the day.<br>(Use infinitive.) ","I still have a little money to buy some food for the day.",
+    
+    
+			10,"I go to school on foot.<br>(Rewrite using \"how\".)", "How do you go to school?",
+    
+    
+			11,"His life inspired us. <br>(Change the voice.)","We were inspired by his life.",
+    
+    
+			12,"And then, all of a sudden, the light filled the room. <br> (Change the voice.)", "And then, all of a sudden, the room was filled with the light.",
+    
+    
+			13, "Strangely, my little kitten is braver than my big dog.<br>(Use positive degree.)","Strangely, my big dog is not so brave as my little kitten.",
+    
+    
+			14, "Undoubtedly he is the best pet dog in the world.<br>(Make a negative sentence.)","Undoubtedly no other pet dog is as good as he.",
+    
+    
+			15,"He will never forget what happened to him. <br> (Turn into a positive sentence.)", "He will always remember what happened to him.",
+    
+			16,"The song started.<br> The baby danced gleefully.<br>(Join into a single sentence.)", "The song having started, the baby danced gleefully.",
+    
+    
+			17,"Soumen may go.<br> Soumen may stay. <br>(Join into a single sentence.)","Soumen may either go or stay.",
+    
+    
+			18,"Your teacher says something. <br>Listen to it minutely.<br> (Join into a single sentence.)", "Listen minutely to what your teacher says.",
+    
+			19,"A barking dog seldom bites.(Join using an adjective clause.)","A dog that barks seldom bites.",
+    
+    
+			20,"I definitely don't want an excuse.<br> (change the voice.)","Definitely an excuse is not wanted by me.",
+    
+    
+			21,"Please, speak in English.<br> (Change the voice.)", "You are requested to speak in English.",
+    
+            22,"Never ever disregard your parents.<br> (Change the voice.)", "Let your parents be never ever disregarded."
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018*/
