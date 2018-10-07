c4e99167a3fba9c5a03d05d3ad148bdb66a52d44
Soumen <soumen@users.noreply.github.com>
2018-10-07 15:25:30 +0530

    copyright-soumen

--- ENGLISH/HS/XII/XII-assorted-grammar-2018/questions.js
+++ ENGLISH/HS/XII/XII-assorted-grammar-2018/questions.js
@@ -1,59 +1,59 @@
 const qa = [ 
-            1,"Shakespeare was a great playwright. <br>He wrote Macbeth.<br>( Join into a complex sentence.)", "Shakespeare was a great playwright who wrote Macbeth.", 
+            1,"She was silent.<br>(Use noun form of 'silent')", "She maintained silence.", 
     
-            2,"I knew him.<br>(change the voice)", "He was known to me.",
+            2,"Then I made a mistake.<br>(change the voice)", "Then a mistake was made by me.",
     
-            3,"What a lovely sight! <br>( Change into an assertive sentence) ", "It is a very lovely sight.",
+            3,"\"Where are you going?\", she asked.<br>(Change the mode of narration) ", "She asked me where I was going.",
         
-            4,"I am quite sure of her success.<br>(Use \"succeed\" and rewrite.)", "I am quite sure that she will succeed.",
+            4,"She said,\"Are you hungry?\"<br>(Change the mode of narration)", "She asked the boy if he was hungry.",
     
-            5,"The teacher said to the students, \"The earth moves round the sun.\" <br>(Change into Indirect Speech.)", "The teacher told the students that the earth moves round the sun.",
+            5,"I wish I were going to Mussoorie.<br> I love the hills.<br>(To complex.)", "I wish I were going to Mussoorie because I love the hills.",
         
-            6,"The sun rises in the east.<br> We all know it.<br> (Use noun clause.)", "We all know that the sun rises in the east.",
+            6,"It takes much time to kill a tree.<br>(Change the voice.)", "Much time is taken to kill a tree.",
         
-            7,"My cat ate a rat. <br>She went away.<br> (Use nominative absolute.)", "Having eaten a rat, my cat went away.",
+            7,"All the necessities were provided for.<br>They were in terms of food, medicine and clothing.<br>(Join.)", "All the necessities in terms of food, medicine and clothing were provided for.",
     
     
-			8,"The old man in our neighbourhood is hard-working. <br> He is honest.<br>(Use a cumulative conjunction.)", "The old man in our neighbourhood is both hard-working and honest.",
+			8,"I was the most important man.<br>(Rewrite the sentence using positive degree of ‘important’.)", "No other man was as important as I was.",
     
     
-			9,"I still have a little money. <br> I can buy some food for the day.<br>(Use infinitive.) ","I still have a little money to buy some food for the day.",
+			9,"Wash your face.<br>(Change the voice.) ","Let your face be washed.",
     
     
-			10,"I go to school on foot.<br>(Rewrite using \"how\".)", "How do you go to school?",
+			10,"A soldier, very young, lies open-mouthed.<br> (Rewrite as a complex sentence.)", "A soldier who is very young, lies open-mouthed.",
     
     
-			11,"His life inspired us. <br>(Change the voice.)","We were inspired by his life.",
+			11,"The Tsar gave the reward to none.<br>(Change the voice.)","None was given the reward by the Tsar.",
     
     
-			12,"And then, all of a sudden, the light filled the room. <br> (Change the voice.)", "And then, all of a sudden, the room was filled with the light.",
+			12,"You have already been answered.<br>(Change the voice.)", "Someone has already answered you.",
     
     
-			13, "Strangely, my little kitten is braver than my big dog.<br>(Use positive degree.)","Strangely, my big dog is not so brave as my little kitten.",
+			13, "The man who had entered the compartment broke into my reverie.<br> (Split into two simple sentences.)","The man entered the compartment. He broke into my reverie.",
     
     
-			14, "Undoubtedly he is the best pet dog in the world.<br>(Make a negative sentence.)","Undoubtedly no other pet dog is as good as he.",
+			14, "I wished to kill you. <br> You have saved my life.<br>(To a compound sentence.)","I wished to kill you but you have saved my life.",
     
     
-			15,"He will never forget what happened to him. <br> (Turn into a positive sentence.)", "He will always remember what happened to him.",
+			15,"Thou art more lovely.<br> Thou art more temperate.<br>(Turn into a compound sentence.)", "Thou art more lovely and temperate.",
     
-			16,"The song started.<br> The baby danced gleefully.<br>(Join into a single sentence.)", "The song having started, the baby danced gleefully.",
-    
-    
-			17,"Soumen may go.<br> Soumen may stay. <br>(Join into a single sentence.)","Soumen may either go or stay.",
+			16,"I am tired of people telling me that I have a pretty face.<br>(Split.)", "I have a pretty face. <br>People tell me this. <br>I am tired of it.",
     
     
-			18,"Your teacher says something. <br>Listen to it minutely.<br> (Join into a single sentence.)", "Listen minutely to what your teacher says.",
-    
-			19,"A barking dog seldom bites.(Join using an adjective clause.)","A dog that barks seldom bites.",
+			17,"From close to the ground will rise curled green twigs.<br>(Split.)","Curled green twigs will rise.<br> They will rise from close to the ground.",
     
     
-			20,"I definitely don't want an excuse.<br> (change the voice.)","Definitely an excuse is not wanted by me.",
+			18,"I would say mine was a very <u>secured</u> childhood.<br> (Correct the error.)", "I would say mine was a very secure childhood.",
+    
+			19,"I am neither a poet <u>or</u> a novelist.<br>(Correct the error.)","I am neither a poet nor a novelist",
     
     
-			21,"Please, speak in English.<br> (Change the voice.)", "You are requested to speak in English.",
+			20,"The boy looked at her.<br> (change the voice.)","She was looked at by the boy.",
     
-            22,"Never ever disregard your parents.<br> (Change the voice.)", "Let your parents be never ever disregarded."
+    
+			21,"Nature's changing course is untrimmed.<br> (Split.)", "Nature's course is changing. <br> The course is untrimmed.",
+    
+            22,"Few girls can <u>resists</u> flattery.<br> (Correct the error.)", "Few girls can resist flattery."
         ];
         const length = qa.length;
 
