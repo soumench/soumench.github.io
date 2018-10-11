84a2abd0696b2f9ddb2f1c1e8eb03cb2ea806b01
Soumen <soumen@users.noreply.github.com>
2018-10-11 21:45:24 +0530

    soumen

--- ENGLISH/SE/IX/assorted-grammar-practice-ix/js_engine/q.js
+++ ENGLISH/SE/IX/assorted-grammar-practice-ix/js_engine/q.js
@@ -21,7 +21,7 @@ var bank = [
     
     ,"Man is mortal.<br>(to Negative Sentence)~No man is immortal."
     
-    ,"Nikola Tesla was a great scientist.<br>(to Interrogative sentence)~Was not Nikola Tesla a great scientist?"
+    ,"Nikola Tesla was a great inventor.<br>(to Interrogative sentence)~Was not Nikola Tesla a great inventor?"
     
     ,"Please obey your parents.<br>(to Assertive sentence)~You are earnestly advised to obey your parents."
     
