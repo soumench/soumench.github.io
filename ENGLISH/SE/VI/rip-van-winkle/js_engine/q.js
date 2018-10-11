aef48b6d8ff67950401204742ca4e4ffb859edb9
Soumen <soumen@users.noreply.github.com>
2018-10-11 22:37:05 +0530

    copyright-soumen

---
+++ ENGLISH/SE/VI/rip-van-winkle/js_engine/q.js
@@ -0,0 +1,44 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Rip Van Winkle <br>by <br>Washington Irving";
+var bank = [
+    "Where was Rip's village located?~The village was located among the Kaatskill Mountains."
+    
+    , "Who taught the children how to fly kites?~ Rip Van Winkle taught the children to fly kites."
+    
+    , "Who helped everyone in their work?~ Rip helped everyone in their work."
+    
+    , "What was the name of Rip's dog?~ The name of the Rip's dog was Wolf."
+    
+    ,"Who was Nicholas Vedder?~Nicholas Vedder, a good friend of Rip, was the landlord of the village."
+    
+    ,"Why did Rip leave his friends? ~Rip left the group because his wife complained."
+    
+    ,"Who made signs for Rip to assist him with the load?~A short old man, with bushy hair and a beard, made a sign for Rip to assist him with the load."
+    
+    ,"What game did the peculiar people play?~ The peculiar people played a game called ninepins."
+    
+    ,"Who recognized Rip when he came back home?~An old woman recognized Rip when he came back home."
+    
+    ,"How long was Rip missing for?~Rip was missing for twenty years."
+    
+    ,"Who looked exactly like Rip's previous self? ~Rip's son looked exactly like his previous self. "
+    
+    ,"What happened to Rip's house when he came back?~Rip found his own house was empty and in ruins."
+    
+    ,"Who assisted children at their sports?~ Rip assisted children at their sports."
+    
+    ,"Who taught kids to shoot marbles?~Rip taught kids to shoot marbles."
+    
+    ,"Who told stories to kids?~Rip told them stories."
+    
+    ,"What happened to Rip's own farm?~It was not in order. His fences fell to pieces, his cows strayed and weeds grew in his fields."
+    
+    ,"Who complained about the idleness of Rip?~Rip's wife complained about his idleness."
+    
+    ,"Who inherited Rip's habits?~Rip's son inherited his habits."
+    
+];
