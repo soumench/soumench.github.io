cc133af4cc1f417002bde9315cb46dd2f9e60c7f
Soumen <soumen@users.noreply.github.com>
2018-10-12 19:18:18 +0530

    copyright-soumen

---
+++ ENGLISH/SE/VI/goodbye-to-the-moon-L8/js_engine/q.js
@@ -0,0 +1,39 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Goodbye to the Moon <br> by <br> Monica Hughes";
+var bank = [
+    "How old is Kepler?~Kepler is fifteen years old."
+    
+    , "Where was Kepler sitting?~ Kepler was sitting in the dark view-room of the space station."
+    
+    , "Who is the Governor of the Moon?~ Kepler's father is the Governor of the moon."
+    
+    , "Who had told stories about Earth to Kepler?~Kepler's mother had told stories about Earth to Kepler."
+    
+    ,"Who was surrounded by the reporters?~Kepler's father was surrounded by the reporters."
+    
+    ,"Which planet seemed so close to Kepler? ~The Earth seemed so close to Kepler."
+    
+    ,"Where was Kepler born?~ Kepler was born on the Moon."
+    
+    ,"What was a luxury on Moon?~ Washing was a luxury on Moon."
+    
+    ,"What was a special delight on Moon?~ Drinking was a special delight on Moon."
+    
+    ,"Which was harder to get than oxygen on Moon?~ Water was harder to get than oxygen on moon."
+    
+    ,"What was the first realization of Kepler on the Earth?~ He realized that he weighed six times his normal weight."
+    
+    ,"What was the expected duration of Kepler's stay on Earth?~Keplet expected to stay as long as six months on Earth."
+    
+    ,"What was home to Kepler's father?~ Moon was home to Kepler's father."
+    
+    ,"What were the things that Kepler wanted to visit?~Kepler wanted to visit Pyramids, the Taj Mahal and the mysterious jungle buildings of the Incas."
+    
+    ,"Why did Kepler's father come to Earth?~ Kepler's father came to settle some differences between the residents of the Earth and the Moon."
+    
+    ,"What was really a new world to Kepler?~ The Earth was really a new place to Kepler."
+];
