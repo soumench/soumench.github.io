eec2e06473085aefe76dbc199f04fc9efcd7c510
Soumen <soumen@users.noreply.github.com>
2018-10-13 01:19:07 +0530

    copyright-soumen

---
+++ ENGLISH/HS/XI/assorted-grammar-practice-xi/js_engine/q.js
@@ -0,0 +1,11 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Goodbye to the Moon <br> by <br> Monica Hughes";
+var bank = [
+    "I will not forget you.(to affirmative)~I will remember you."
+    
+    ,"I believe that love begins at home.<br>(Split into two simple sentences.)~Love begins at home.<br> I believe it."
+];
