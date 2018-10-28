5f208253f14e86fa1b27243b5f5e405faa9d90ae
Soumen <soumen@users.noreply.github.com>
2018-10-28 14:39:27 +0530

    soumen

--- ENGLISH/HS/XI/assorted-grammar-practice-xi/js_engine/q.js
+++ ENGLISH/HS/XI/assorted-grammar-practice-xi/js_engine/q.js
@@ -3,9 +3,63 @@ Copyright: Soumen Chakraborty 2018
 https://soumen-here.blogspot.com/
 Strictly for Educational and Non-commercial Purpose
 */
-heading.innerHTML="Goodbye to the Moon <br> by <br> Monica Hughes";
+heading.innerHTML="Assorted Questions-CLASS-XI";
 var bank = [
-    "I will not forget you.(to affirmative)~I will remember you."
+     "I will show you something nice.(to complex)~I will show you something which is nice."
     
-    ,"I believe that love begins at home.<br>(Split into two simple sentences.)~Love begins at home.<br> I believe it."
+    ,"Who was Ben Price?~a detective"
+    
+    ,"Who was Jimmy Valentine?~no hint."
+    
+    ,"Who was Sidda?~ a servant"
+    
+    ,"How did Sidda make Leela happy?~ no hint."
+    
+    ,"Who was Leela? ~ no hint"
+    
+    ,"Who was Sir Mohan Lal?~ no hint"
+    
+    ,"Who was Mother Teresa?~ no hint"
+    
+    ,"Where was Lachmi sitting?~on a small grey trunk"
+    
+    ,"What were the names of the two English soldiers?~Bill and Jim"
+    
+    ,"Who composed the poem 'Meeting at Night'?~Robert Browning"
+    
+    ,"Where did Leela's mother find the chain?~inside the tamarind pot in the kitchen"
+    
+    ,"What is the colour of the sea in 'Meeting at Night'?~grey"
+    
+    ,"Who is the destroyer of the life of the rose?~an invisible worm"
+    
+    ,"What business did Jimmy take in Elmore?~shoe business"
+    
+    ,"How did Mother Teresa fight against abortion?~by adoption"
+    
+    ,"The warden gave him a cigar.(Change the voice.)~He was given a cigar by the warden."
+    
+    ,"Please tell us something that we will remember.(to simple)~Please tell us something to remember."
+    
+    ,"'Tell me a story, mother', Leela Said.(Change the mode of narration.)~Leela requested her mother to tell a story."
+    
+    ,"How many professors from the USA came to visit Mother Teresa?~fourteen"
+    
+    ,"Whom did Jimmy rescue from the vault?~Agatha"
+    
+    ,"Who was Dromio of Syracuse?~ a servant"
+    
+    ,"How many witches did Macbeth meet?~ three"
+    
+    ,"Open the door.(Change the voice.)~Let the door be opened."
+    
+    ,"The mirror was obviously made in India.(to Complex)~It was obvious that the mirror was made in India."
+    
+    ,"I believe that love begins at home.(Split)~no hint"
+    
+    ,"I love God but I don't love my neighbour.(to complex)~ Though I love God I don't love my neighbour."
+    
+    ,"I seems he is an old criminal.(to simple)~Seemingly he is an old criminal."
+    
+    ,"It was a complete set.(to complex)~It was a set that was complete."    
 ];
