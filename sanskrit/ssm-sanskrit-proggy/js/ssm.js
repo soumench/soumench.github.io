f02ef5eab22f13b99908f378a23640b3702b431a
Soumen <soumen@users.noreply.github.com>
2018-11-01 00:31:32 +0530

    soumen

--- sanskrit/ssm-sanskrit-proggy/js/ssm.js
+++ sanskrit/ssm-sanskrit-proggy/js/ssm.js
@@ -32,3 +32,48 @@ const en = e("en");
 const qz = e("qz");
 const qz2 = e("qz2");
 const nqz = e("noquiz");
+
+var perm=[0,1,2];
+
+var dn = 0,  vn = 0,   on = 0;
+
+var ode, ove, ooe;
+
+var ndn, nvn, non;
+
+
+function flip() {
+    showenglish = !showenglish;
+    if (showenglish) {
+        pass(grid[0], edoers, 0);
+        pass(grid[1], everbs, 1);
+        pass(grid[2], eobjects, 2);
+        qz.setAttribute('class', "san");
+    } else {
+        pass(grid[0], doers, 0);
+        pass(grid[1], verbs, 1);
+        pass(grid[2], objects, 2);
+        qz.setAttribute('class', "eng");
+    }
+    output();
+}
+
+
+
+function quiz() {
+    if (!QUIZMODE) QUIZMODE = true;
+    do {
+        ndn = Math.floor((Math.random() * doers.length));
+        nvn = Math.floor((Math.random() * verbs.length));
+        non = Math.floor((Math.random() * objects.length));
+    } while (ndn == dn || nvn == vn || non == on);
+
+    swapr();
+}
+
+function noquiz() {
+    if (!QUIZMODE) return;
+    QUIZMODE = false;
+    qz.innerHTML = "&nbsp;";
+    qz2.innerHTML = "&nbsp;";
+}
