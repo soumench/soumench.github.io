67ced9c177b6fa5863b1d8366de1b6066cffb3f2
Soumen <soumen@users.noreply.github.com>
2018-11-19 22:57:37 +0530

    Created by Soumen

--- MY_SMALL_WEB_APPLICATIONS/atm-parts-of-speech/js/posrepo.js
+++ MY_SMALL_WEB_APPLICATIONS/atm-parts-of-speech/js/posrepo.js
@@ -1,12 +1,18 @@
 /*Created by Soumen Chakraborty - 2018 - Strictly For Educational, Non-profit and Non-Commercial Purpose */
 var newvalue; var oldvalue;
 var transactionstartmsg=`<br><br><br>Transaction in progress.<br><br>Please wait....`; 
-var transactionfinished=`<br><br><br>Transaction complete. <br><br> Try again.`;
+var transactionfinished=`<br><br><br>Transaction complete. <br><br><br> You may want to try again.`;
 function nounactivity(){Noun.style.color="red";
 const nouns=[`Soumen <br><br><br>Soumen is a teacher.`
-             ,"cat<br><br><br>The cat purrs."
-             ,"horse<br><br><br>The horse runs fast."
+             ,"Cat<br><br><br>The cat purrs."
+             ,"Horse<br><br><br>The horse runs fast."
+             ,"Anger<br><br><br>Anger is not a good thing."
              ,"dog<br><br><br>A barking dog seldom bites."
+             ,"Honesty<br><br><br>Honesty is the best policy."
+             ,"Adversity<br><br><br>Adversity offers a scope of introspection."
+             ,"Kolkata<br><br><br>Kolkata is a very congested city."
+             ,"Smoking<br><br><br>Don't smoke please. Smoking kills."
+             ,"School<br><br><br>Do you love your school?"
 ]; wordOutput(nouns);}
 function pronounactivity(){ Pronoun.style.color="red";
 const pronouns=[`He <br><br><br>He is smart.`
@@ -15,9 +21,11 @@ const pronouns=[`He <br><br><br>He is smart.`
              ,"We<br><br><br>We like to help all."
              ,"I<br><br><br>am from India."
              ,"They<br><br><br>They played really well."
+             ,"Those<br><br><br>Soumen devoted himself to music in those days."
              ,"It<br><br><br>It is so small!"
              ,"This<br><br><br>This is the toy I made."
              ,"That<br><br><br>That is our house."
+            ,"These<br><br><br>Did you double-check everything?"                
 ];wordOutput(pronouns);}
 function verbactivity(){Verb.style.color="red";
 const verbs=[`Play<br><br><br>Play is the highest form of research.`
@@ -49,7 +57,7 @@ const verbs=[`Play<br><br><br>Play is the highest form of research.`
 ]; wordOutput(verbs);}
 function adverbactivity(){Adverb.style.color="red";
 const adverbs=[`badly<br><br><br>She did not run badly.`
-             ,"Quickly<br><br><br>He finished his hometask quickly."
+             ,"Quickly<br><br><br>He finished his home-task quickly."
              ,"Loudly<br><br><br>My brother sings loudly in the shower."
              ,"Well<br><br><br>Even pet dog can respond well."
              ,"impatiently<br><br><br>We all wait impatiently for the mid-day meal."
@@ -57,40 +65,47 @@ const adverbs=[`badly<br><br><br>She did not run badly.`
             ,"Incredibly<br><br><br>My cat is incredibly happy to see me when I come back home."
 ];wordOutput(adverbs);}
 function adjectiveactivity(){Adjective.style.color="red";
-const adjectives=[`badly<br><br><br>She did not run badly.`
-             ,"Quickly<br><br><br>He finished his hometask quickly."
-             ,"Loudly<br><br><br>My brother sings loudly in the shower."
-             ,"Well<br><br><br>Even pet dog can respond well."
-             ,"impatiently<br><br><br>We all wait impatiently for the mid-day meal."
-            ,"Seriously<br><br><br>Please study seriously and attentively."
-            ,"Incredibly<br><br><br>My cat is incredibly happy to see me when I come back home."
+const adjectives=[`Adamant<br><br><br>He is adamant about his point of view.`
+             ,"Didactic<br><br><br>His work became didactic after his religious conversion."
+             ,"Concomitant<br><br><br>His higher education brought a concomitant positive change in his behaviour."
+             ,"Egregious<br><br><br>Some egregious spelling errors completely ruined his composition."
+             ,"Insolent<br><br><br>Insolent behaviour will not be tolerated at all."
+            ,"Obnoxious<br><br><br>His obnoxious words offended all."
+            ,"Judicious<br><br><br>A judicious criticism is always welcome."
 ]; wordOutput(adjectives);}
 function prepositionactivity(){Preposition.style.color="red";
 const prepositions=[`badly<br><br><br>She did not run badly.`
-             ,"Quickly<br><br><br>He finished his hometask quickly."
-             ,"Loudly<br><br><br>My brother sings loudly in the shower."
-             ,"Well<br><br><br>Even pet dog can respond well."
-             ,"impatiently<br><br><br>We all wait impatiently for the mid-day meal."
-            ,"Seriously<br><br><br>Please study seriously and attentively."
-            ,"Incredibly<br><br><br>My cat is incredibly happy to see me when I come back home."
+             ,"On<br><br><br>My cat loves to sit on the chair."
+             ,"Under<br><br><br>My brother hides under the table."
+             ,"Over<br><br><br>Daddy drove over the Howrah bridge."
+             ,"By<br><br><br>A group of people were sitting by the tree."
+            ,"To<br><br><br>This house belongs to my uncle."
+            ,"At<br><br><br>I found a nice chank at the beach that day."
+            ,"From<br><br><br>I received my order from Amazon yesterday."
+            ,"For<br><br><br>Mom cooked two eggs for breakfast."
+            ,"Down<br><br><br>He went down the street with his bag."
+            ,"Since<br><br><br>I loved the voice of Kishore Kumar since I was a kid."        
 ]; wordOutput(prepositions);}
 function conjunctionactivity(){Conjunction.style.color="red";
-const conjunctions=[`badly<br><br><br>She did not run badly.`
-             ,"Quickly<br><br><br>He finished his hometask quickly."
-             ,"Loudly<br><br><br>My brother sings loudly in the shower."
-             ,"Well<br><br><br>Even pet dog can respond well."
-             ,"impatiently<br><br><br>We all wait impatiently for the mid-day meal."
-            ,"Seriously<br><br><br>Please study seriously and attentively."
-            ,"Incredibly<br><br><br>My cat is incredibly happy to see me when I come back home."             
+const conjunctions=[`Although<br><br><br>Although he is poor he is still honest.`
+             ,"And<br><br><br>I like both tea and coffee."
+             ,"But<br><br><br>I gave the beggar a piece of bread but he refused it."
+             ,"Because<br><br><br>He is sneaking around in the kitchen for cake because he loves it."
+             ,"Either<br><br><br>We can either go to a movie or a park."
+             ,"In case<br><br><br>Take the mobile with you in case you miss the train."
+             ,"Or<br><br><br>Do you like biryani or dosa?"
+             ,"Unless<br><br><br>You cannot sleep well unless you stope playing with your phone." 
+             ,"Whether<br><br><br>Accept the truth whether you like it or not."
+             ,"Even<br><br><br>Grandpa loves to walk in the morning even in winter."
+             ,"Even if<br><br><br>Sometimes things do not work even if you try your best."
 ]; wordOutput(conjunctions);}
 function interjectionactivity(){Interjection.style.color="red";
-const interjections=[`badly<br><br><br>She did not run badly.`
-             ,"Quickly<br><br><br>He finished his hometask quickly."
-             ,"Loudly<br><br><br>My brother sings loudly in the shower."
-             ,"Well<br><br><br>Even pet dog can respond well."
-             ,"impatiently<br><br><br>We all wait impatiently for the mid-day meal."
-            ,"Seriously<br><br><br>Please study seriously and attentively."
-            ,"Incredibly<br><br><br>My cat is incredibly happy to see me when I come back home."
+const interjections=[`Aah<br><br><br>Aah, that is so amazing!`
+             ,"Alas<br><br><br>Alas! I am undone."
+             ,"Hurrah<br><br><br>Hurrah! India has won the match!"
+             ,"Oh dear<br><br><br>Oh dear! My cat scratched me in anger."
+             ,"Yoo-hoo<br><br><br>Yoo-hoo! Are you there?"
+             ,"Blistering bernacles and thundering typhoons<br><br><br>Blistering bernacles and thundering typhoons! Where is Professor?"            
 ]; wordOutput(interjections);}
 function wordOutput(mylist){shuffle(mylist); var len = mylist.length;
     disableBtns(); var repeat = setInterval(dothis,displaydelay); var i=0;
