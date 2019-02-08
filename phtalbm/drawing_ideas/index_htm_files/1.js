8cb67cf33f1f6e997aade3cb9e3ec8d1463553d2
Soumen <soumen@users.noreply.github.com>
2019-02-08 18:22:04 +0530

    ig update

---
+++ phtalbm/drawing_ideas/index_htm_files/1.js
@@ -0,0 +1,309 @@
+window.local_imageGallerySimple_v11_1_data = [
+{
+ border: "4",
+ cols: "5",
+ effect: "3",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/51.jpg"
+},
+{
+ image: "index_htm_files/52.jpg"
+},
+{
+ image: "index_htm_files/53.jpg"
+},
+{
+ image: "index_htm_files/54.jpg"
+},
+{
+ image: "index_htm_files/55.jpg"
+},
+{
+ image: "index_htm_files/56.jpg"
+},
+{
+ image: "index_htm_files/57.jpg"
+},
+{
+ image: "index_htm_files/58.jpg"
+},
+{
+ image: "index_htm_files/59.jpg"
+},
+{
+ image: "index_htm_files/60.jpg"
+},
+{
+ image: "index_htm_files/61.jpg"
+},
+{
+ image: "index_htm_files/62.jpg"
+},
+{
+ image: "index_htm_files/63.jpg"
+},
+{
+ image: "index_htm_files/64.jpg"
+},
+{
+ image: "index_htm_files/65.jpg"
+},
+{
+ image: "index_htm_files/66.jpg"
+},
+{
+ image: "index_htm_files/67.jpg"
+},
+{
+ image: "index_htm_files/68.jpg"
+},
+{
+ image: "index_htm_files/69.jpg"
+},
+{
+ image: "index_htm_files/70.jpg"
+},
+{
+ image: "index_htm_files/71.jpg"
+},
+{
+ image: "index_htm_files/72.jpg"
+},
+{
+ image: "index_htm_files/73.jpg"
+},
+{
+ image: "index_htm_files/74.jpg"
+},
+{
+ image: "index_htm_files/75.jpg"
+},
+{
+ image: "index_htm_files/76.jpg"
+},
+{
+ image: "index_htm_files/77.jpg"
+},
+{
+ image: "index_htm_files/78.jpg"
+},
+{
+ image: "index_htm_files/79.jpg"
+},
+{
+ image: "index_htm_files/80.jpg"
+},
+{
+ image: "index_htm_files/81.jpg"
+},
+{
+ image: "index_htm_files/82.jpg"
+},
+{
+ image: "index_htm_files/83.jpg"
+},
+{
+ image: "index_htm_files/84.jpg"
+},
+{
+ image: "index_htm_files/85.jpg"
+},
+{
+ image: "index_htm_files/86.jpg"
+},
+{
+ image: "index_htm_files/87.jpg"
+},
+{
+ image: "index_htm_files/88.jpg"
+},
+{
+ image: "index_htm_files/89.jpg"
+},
+{
+ image: "index_htm_files/90.jpg"
+},
+{
+ image: "index_htm_files/91.jpg"
+},
+{
+ image: "index_htm_files/92.jpg"
+},
+{
+ image: "index_htm_files/93.jpg"
+},
+{
+ image: "index_htm_files/94.jpg"
+},
+{
+ image: "index_htm_files/95.jpg"
+},
+{
+ image: "index_htm_files/96.jpg"
+},
+{
+ image: "index_htm_files/97.jpg"
+},
+{
+ image: "index_htm_files/98.jpg"
+},
+{
+ image: "index_htm_files/99.jpg"
+},
+{
+ image: "index_htm_files/100.jpg"
+},
+{
+ image: "index_htm_files/101.jpg"
+},
+{
+ image: "index_htm_files/102.jpg"
+},
+{
+ image: "index_htm_files/103.jpg"
+},
+{
+ image: "index_htm_files/104.jpg"
+},
+{
+ image: "index_htm_files/105.jpg"
+},
+{
+ image: "index_htm_files/106.jpg"
+},
+{
+ image: "index_htm_files/107.jpg"
+},
+{
+ image: "index_htm_files/108.jpg"
+},
+{
+ image: "index_htm_files/109.jpg"
+},
+{
+ image: "index_htm_files/110.jpg"
+},
+{
+ image: "index_htm_files/111.jpg"
+},
+{
+ image: "index_htm_files/112.jpg"
+},
+{
+ image: "index_htm_files/113.jpg"
+},
+{
+ image: "index_htm_files/114.jpg"
+},
+{
+ image: "index_htm_files/115.jpg"
+},
+{
+ image: "index_htm_files/116.jpg"
+},
+{
+ image: "index_htm_files/117.jpg"
+},
+{
+ image: "index_htm_files/118.jpg"
+},
+{
+ image: "index_htm_files/119.jpg"
+},
+{
+ image: "index_htm_files/120.jpg"
+},
+{
+ image: "index_htm_files/120.jpg"
+},
+{
+ image: "index_htm_files/121.jpg"
+},
+{
+ image: "index_htm_files/122.jpg"
+},
+{
+ image: "index_htm_files/123.jpg"
+},
+{
+ image: "index_htm_files/124.jpg"
+},
+{
+ image: "index_htm_files/125.jpg"
+},
+{
+ image: "index_htm_files/126.jpg"
+},
+{
+ image: "index_htm_files/127.jpg"
+},
+{
+ image: "index_htm_files/128.jpg"
+},
+{
+ image: "index_htm_files/129.jpg"
+},
+{
+ image: "index_htm_files/130.jpg"
+},
+{
+ image: "index_htm_files/131.jpg"
+},
+{
+ image: "index_htm_files/132.jpg"
+},
+{
+ image: "index_htm_files/133.jpg"
+},
+{
+ image: "index_htm_files/134.jpg"
+},
+{
+ image: "index_htm_files/135.jpg"
+},
+{
+ image: "index_htm_files/136.jpg"
+},
+{
+ image: "index_htm_files/137.jpg"
+},
+{
+ image: "index_htm_files/138.jpg"
+},
+{
+ image: "index_htm_files/139.jpg"
+},
+{
+ image: "index_htm_files/140.jpg"
+},
+{
+ image: "index_htm_files/141.jpg"
+},
+{
+ image: "index_htm_files/142.jpg"
+},
+{
+ image: "index_htm_files/143.jpg"
+},
+{
+ image: "index_htm_files/143.jpg"
+},
+{
+ image: "index_htm_files/144.jpg"
+},
+{
+ image: "index_htm_files/145.jpg"
+},
+{
+ image: "index_htm_files/146.jpg"
+},
+{
+ image: "index_htm_files/147.jpg"
+}
+];
