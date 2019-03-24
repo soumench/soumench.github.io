199ee694dbb66829544516299fc6287a9403d729
Soumen <soumen@users.noreply.github.com>
2019-03-24 13:59:03 +0530

    enixallsinaday

---
+++ en/enixallsinday/js/count.js
@@ -0,0 +1,11 @@
+function increament(){
+	i++;
+	if(i>=(list.length/5)+2){i=0;}           
+	/*show();*/
+}
+
+/*function decreament(){
+	i--;
+	if(i< 0){ i=(list.length/5)-1;}           
+	show();
+}      */ 
