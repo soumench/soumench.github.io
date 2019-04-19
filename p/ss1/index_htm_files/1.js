29f1c73a54bb600ad2da9e756440af3a38ef3467
Soumen <soumen@users.noreply.github.com>
2019-04-19 22:08:00 +0530

    upd

---
+++ p/ss1/index_htm_files/1.js
@@ -0,0 +1,87 @@
+window.local_imageGallerySimple_v11_0_data = [
+{
+ border: "4",
+ cols: "3",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/14.jpg"
+},
+{
+ image: "index_htm_files/15.jpg"
+},
+{
+ image: "index_htm_files/16.jpg"
+},
+{
+ image: "index_htm_files/17.jpg"
+},
+{
+ image: "index_htm_files/18.jpg"
+},
+{
+ image: "index_htm_files/19.jpg"
+},
+{
+ image: "index_htm_files/48.jpg"
+},
+{
+ image: "index_htm_files/49.jpg"
+},
+{
+ image: "index_htm_files/50.jpg"
+},
+{
+ image: "index_htm_files/51.jpg"
+},
+{
+ image: "index_htm_files/52.jpg"
+},
+{
+ image: "index_htm_files/53.jpg"
+},
+{
+ image: "index_htm_files/54.jpg"
+},
+{
+ image: "index_htm_files/55.jpg"
+},
+{
+ image: "index_htm_files/56.jpg"
+},
+{
+ image: "index_htm_files/57.jpg"
+},
+{
+ image: "index_htm_files/58.jpg"
+},
+{
+ image: "index_htm_files/59.jpg"
+},
+{
+ image: "index_htm_files/60.jpg"
+},
+{
+ image: "index_htm_files/61.jpg"
+},
+{
+ image: "index_htm_files/62.jpg"
+},
+{
+ image: "index_htm_files/63.jpg"
+},
+{
+ image: "index_htm_files/64.jpg"
+},
+{
+ image: "index_htm_files/65.jpg"
+},
+{
+ image: "index_htm_files/66.jpg"
+}
+];
