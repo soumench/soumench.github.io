05057fdc90727dcfe59c5998c5be15e10dc6b9cc
Soumen <soumen@users.noreply.github.com>
2019-09-20 21:12:46 +0530

    asfasdadahh

---
+++ en/x_en_snl/qsource/q.js
@@ -0,0 +1,100 @@
+const topic_name =" M.C.Q. on \"The Snail\""; 
+const list=[
+            "Who is a hymndodist?",
+	 		"A composer of prose.",
+	 		"A composer of drama.",
+	 		"A composer of hymns.",
+	 		"c",
+	 
+	 
+            "William Cowper was",
+	 		"a German Poet.",
+	 		"a French Poet.",
+	 		"an English Poet.",
+	 		"c",
+	
+	        "The snail usually sticks to.",
+	 		"glass, pillar, dome.",
+	 		"grass, leaf, fruit, wall.",
+	 		"water, mud, staircase.",
+	 		"b",
+	 		
+	 		"The snail ",
+	 		"never eats.",
+	 		"eats faster.",
+	 		"seldom eats.",
+	 		"b",
+	 		
+	 		"Snail's house is",
+	 		"his shell",
+	 		"his area.",
+	 		"everywhere.",
+	 		"a",
+	 		
+	 		"The snail is not afraid of",
+	 		"falling.",
+	 		"fighting.",
+	 		"eating.",
+	 		"a",
+	 		
+	 		"Betides means",
+	 		"besides.",
+	 		"to happen / to befall.",
+	 		"by the side of.",
+	 		"b",
+	 
+	 		"Chatells means",
+	 		"pals.",
+	 		"chandelier.",
+	 		"personal belongings.",
+	 		"c",
+	 
+	 		"The snail shrinks into his house ",
+	 		"with displeasure.",
+	 		"with pleasure.",
+	 		"very gladly.",
+	 		"a",
+	 
+	 		"The snail dwells",
+	 		"with all.",
+	 		"alone.",
+	 		"with selected friends.",
+	 		"b",
+	 
+	 		"Imminent means",
+	 		"inmates.",
+	 		"indomitable.",
+	 		"likely to happen soon.",
+	 		"c",
+	 
+	 		"Usually the snail is",
+	 		"satisfied.",
+	 		"dissatisfied.",
+	 		"stoic.",
+	 		"a",
+	 
+	 		"The snail has a",
+	 		"destructive power.",
+	 		"self-collecting power.",
+	 		"constructive power.",
+	 		"b",
+	 
+	 		"The snail leads a",
+	 		"luxurious life.",
+	 		"miserable life.",
+	 		"self-sufficient life.",
+	 		"c",
+	 
+	 		"The world of snail is",
+	 		"real huge.",
+	 		"small yet self-contained.",
+	 		"all encompassing.",
+	 		"b",
+			
+			"A snail has horns.",
+	 		"False.",
+	 		"True.",
+	 		"I don't know.",
+	 		"b"
+			
+        ];
