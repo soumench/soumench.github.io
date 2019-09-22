a4dca20008cb246e7fb79b90a213d7a7eb4ff695
Soumen <soumen@users.noreply.github.com>
2019-09-22 20:24:40 +0530

    yi kjhjhl

---
+++ en/v_ann_19_sugstn/js_engine/q.js
@@ -0,0 +1,65 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Assorted Questions-CLASS-XI";
+var bank = [
+     "Who was Rakesh Sharma?~ The first Indian astronaut in space."
+    
+    ,"Who was Mrs. Gandhi?~ the then Prime minister."
+    
+    ,"What is the meaning of 'philosophos'?~ lover of wisdom."
+    
+    ,"What is the opposite in meaning of 'before'?~ after"
+    
+    ,"What is the capital of India?~ New Delhi."
+    
+    ,"I teach in a school. Who am I? ~ I am a teacher."
+    
+    ,"He is a good boy.(Find out the adjective.)~ good"
+    
+    ,"Past tense of give?~ gave"
+    
+    ,"Past tense of come?~came"
+    
+    ,"What do you do when you get up from bed?~no hint"
+    
+    ,"When do you go to bed?~ no hint"
+    
+    ,"My grand-dad walks slowly( Add an adverb)~ slowly"
+    
+    ,"Opposite in meaning of 'never'?~always"
+    
+    ,"Opposite of 'love'~hate"
+    
+    ,"What are the citizens of Greece called?~Greek"
+    
+    ,"I have/has a pet cat.( Choose correct form.)~ have"
+    
+    ,"Opposite of 'come'~'go'."
+    
+    ,"Who gave a book to Barun?~ Barun's mother."
+    
+    ,"What was the name of the space shuttle?~ Soyuj T-11."
+    
+    ,"Rakesh Sharma spent some nine days in space. True or false? ~ False"
+    
+    ,"My father is a/an honest man. ( Choose the correct article.)~ an"
+    
+    ,"Who was Plato?~ a Greek Philosopher."
+    
+    ,"How many colours are there in the rainbow?~ seven"
+    
+    ,"What is the name of our country? ~ India."
+    
+    ,"What is the name of your district? ~ no hint."
+    
+    ,"What is the name of your town/ village? ~no hint"
+    
+    ,"What is your father? ( Your father's profession)~ no hint."
+    
+    ,"What is the name of your school?~ no hint."
+    
+    ,"Full form of I.S.R.O ~ Indian Space Research Organization."    
+];
