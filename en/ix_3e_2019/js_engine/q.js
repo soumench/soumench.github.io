edd0d9039033cf3d9767991a0b363ed0f003a31c
Soumen <soumen@users.noreply.github.com>
2019-09-24 20:12:46 +0530

    dgadgawrwrurwr

---
+++ en/ix_3e_2019/js_engine/q.js
@@ -0,0 +1,83 @@
+/*
+Copyright: Soumen Chakraborty 2018-19
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Assorted Questions (CLASS-IX)";
+var bank = [
+     "Who was Roninson Crusoe? ~ A shipwrecked traveller."
+    
+    ,"What is the noun form of 'perfect'? ~ perfection."
+    
+    ,"What is the comparative degree of 'good'?~ better."
+    
+    ,"What is the opposite in meaning of 'before'?~ after"
+    
+    ,"What is the capital of India?~ New Delhi."
+    
+    ,"He ___(has been/have been) waiting for an hour.(Choose correct form.) ~ has been"
+    
+    ,"He is a good boy.(Find out the adjective.)~ good"
+    
+    ,"Past tense of give?~ gave"
+    
+    ,"Past tense of come?~came"
+    
+    ,"He is___(going/went) to Kolkata tomorrow. ~ going"
+    
+    ,"When do you go to bed?~ no hint"
+    
+    ,"My grand-dad walks.( Add an adverb)~ slowly"
+    
+    ,"Opposite in meaning of 'never'?~always"
+    
+    ,"Opposite of 'love'~hate"
+    
+    ,"What are the citizens of Greece called?~Greek"
+    
+    ,"I have/has a pet cat.( Choose correct form.)~ have"
+    
+    ,"Opposite of 'come'~'go'."
+    
+    ,"I lost my cat. I have found him. (join) ~ I have found my lost cat."
+    
+    ,"She is pretty. She is intelligent.(join) ~ She is pretty and intelligent."
+    
+    ,"'Where is the newspaper?', dad said. (Change the mode of narration.)~ no hint"
+    
+    ,"My father is a/an honest man. ( Choose the correct article.)~ an"
+    
+    ,"Paltu said,'I have done it'.(Narration)~ Paltu said that he had done it."
+    
+    ,"How many colours are there in the rainbow?~ seven"
+    
+    ,"What is the name of our country? ~ India."
+    
+    ,"What is the name of your district? ~ no hint."
+    
+    ,"What is the name of your town/ village? ~no hint"
+    
+    ,"What is your father? ( Your father's profession)~ no hint."
+    
+    ,"What is the name of your school?~ no hint."
+    
+    ,"Full form of I.S.R.O ~ Indian Space Research Organization."
+    
+    ,"What is Chandrayaan2 ?  ~ the second lunar exploration mission developed by ISRO."
+    
+    ,"How paper is made?  ~ no hint"
+    
+    ,"Who was Einstein?  ~ a famous physicist who developed the theory of relativity."
+    
+    ,"What did wind do to the second ship?  ~ (hint:'the wind hunted it like a beast To anchor in captivity.')"
+    
+    ,"Find out the describing words used in the poem 'The North Ship'~ no hint"
+    
+    , "Who was Stephen Hawking? ~ no hint"
+    
+    ,"How can we save water? ~ Discuss with your friends."
+    
+    ,"What is global warming? ~ Share your findings with your friends."
+    
+    ,"What is e-waste? ~ Share your findings with your friends."
+];
