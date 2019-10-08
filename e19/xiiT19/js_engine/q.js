e5fadd3a0c1bdb9c49e250e67a824a660609d61a
Soumen <soumen@users.noreply.github.com>
2019-10-08 14:24:01 +0530

    aagdueknxn

---
+++ e19/xiiT19/js_engine/q.js
@@ -0,0 +1,62 @@
+/*
+Copyright: Soumen Chakraborty 2018-19
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="English Questions - Class XII";
+var bank = [
+     "What were the three questions ? ~ no hint "
+    
+    ,"Did Mrs. Jones show any motherly feeling? ~ no hint "
+    
+    ,"What message did the poet want to give in the poem 'Asleep in the Valley  ~ hint:futility of war "
+    
+    ,"How did the poet depict different seasons in the poem 'The Poetry of Earth'? ~ no hint"
+    
+    ,"How did Shakespeare hope to immortalize his friend's beauty?  ~ no hint"
+    
+    ,"Why did Lomov want to marry?   ~ no hint"
+    
+    ,"Who was described as a 'love sick cat' in 'The Proposal'? ~ Natalya"
+    
+    ,"Did Chubukov behave like a sensible father? ~ no hint"
+    
+    ,"What was the profession of Mrs. Jones? ~ no hint"
+    
+    ,"What is the name of Dr. Kalam's birth-place?  ~ no hint "
+    
+    ,"Where did the hermit live? ~ no hint "
+    
+    ,"What is described as 'gentle and guile'? ~ no hint"
+    
+    ,"Where does the grasshopper rest? ~ no hint "
+    
+    ,"What was the name of Natalya's dog? ~ Squeezer"
+    
+    ,"What was the name of Lomov's dog? ~ Guess"
+    
+    ,"What is a farce? ~ hint: a light, humourous play that uses buffoonery"
+    
+    ,"What did Kalam’s father tell him about the relevance of prayer? ~ no hint"
+    
+    ,"“You have already been answered.”-What had already been answered? ~ no hint"
+    
+    ,"Do you think that the title of the poem 'On Killing of a Tree' is apt? ~ no hint"
+    
+    ,"Why does Keats feel that the poetry of earth is never dead?~ no hint"
+    
+    ,"The title ‘Tsar’ belonged to? ~ The King of Russia"
+    
+    ,"The word ‘pocket book’ in the story “Thank You Ma’am” means ~ no hint"
+    
+    ,"When did the father of A.P.J. Abdul Kalam get up from bed? ~  no hint"
+    
+    ,"Name a distinguished friend of Kalam’s father. ~ no hint"
+    
+    ,"Lomov was called a ‘scare crow’ by~ Lomov"
+    
+    ,"What makes the summer musical?~ no hint"
+    
+    ,"What according to the hermit is the most important time? ~  now"
+        
+];
