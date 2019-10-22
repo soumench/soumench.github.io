958cc661b6ad4355e6d0c9c03fd1f9d6f4442c46
Soumen <soumen@users.noreply.github.com>
2019-10-23 05:25:40 +0530

    adgadgadgdgadadg

--- p/gc/script/calculate.js
+++ p/gc/script/calculate.js
@@ -16,8 +16,9 @@ function gstUpdate(){
                 gst = parseFloat(e("gst").value);               
                 cost = cost ? cost : 0; /*avoid NaN*/
                 gst = gst ? gst :0;
-                cgst.innerHTML=gst/2+" \%"+" ( "+cost*((gst/2)/100)+" INR)";
-                sgst.innerHTML=gst/2+" \%"+" ( "+cost*((gst/2)/100)+" INR)";
+                var gstCalc = roundToTwo(cost*((gst/2)/100));
+                cgst.innerHTML=gst/2+" \% ( "+ gstCalc +" INR)";
+                sgst.innerHTML=gst/2+" \% ( "+ gstCalc +" INR)";
                 gstTotal.innerHTML=roundToTwo(cost*gst/100)+" INR)";
                 total.innerHTML=Math.round(cost+cost*gst/100,0)+" INR";
 }  
