ac835f14cb6b6cf7d2d41634ab98a25ce8129b4a
Soumen Ch <soumench@users.noreply.github.com>
2020-04-19 01:44:59 +0530

    ewyhsfhsfsfhs

---
+++ e20/ge8q/qsource/q.js
@@ -0,0 +1,143 @@
+const topic_name =" M.C.Q. on \"THE GREAT ESCAPE\""; 
+const list=[
+            "Sugata Bose is a ",
+	 		"historian and a professor.",
+	 		"an American author.",
+	 		"a French author.",
+	 		"a",
+	 
+	 
+            "Netaji was one of the",
+	 		"less celebrated politicians.",
+	 		"not so famous leaders.",
+	 		"most famous nationalist leaders.",
+	 		"c",
+	
+	        "Sisir was ",
+	 		"his brother.",
+	 		"his nephew.",
+	 		"his servant.",
+	 		"b",
+	 		
+	 		"Subhas's nephew Sisir ",
+	 		"refused to do the task.",
+	 		"agreed to do the task.",
+	 		"was indifferent to the task.",
+	 		"b",
+	 		
+	 		"The task was to help plan and carry out",
+	 		"Subhas's escape from India.",
+	 		"Subhas's escape from Britain.",
+	 		"Subhas's escape from Germany.",
+	 		"a",
+	 		
+	 		"Subhas had a residence at ",
+	 		"Elgin Road.",
+	 		"Woodburn Park.",
+	 		"Park Street.",
+	 		"a",
+	 		
+	 		"It was normal for a nephew",
+	 		"to hide his ailing uncle.",
+	 		"to visit his ailing uncle.",
+	 		"to turn a deaf ear to his uncle's words.",
+	 		"b",
+	 
+	 		"Sisir owned a ",
+	 		"Lamborghini Roadster.",
+	 		"Bugatti Divo.",
+	 		"German car.",
+	 		"c",
+	 
+	 		"Sisir helped Subhas listen to ",
+	 		"foreign broadcasts.",
+	 		"local broadcasts.",
+	 		"talk shows on TV.",
+	 		"a",
+	 
+	 		"Akbar Shah was a ",
+	 		"post man.",
+	 		"freedom fighter.",
+	 		"politician.",
+	 		"b",
+	 
+	 		"The calling cards read",
+	 		"Sisir Bose.",
+	 		"Subhas Chandra Bose.",
+	 		"Md. Ziauddin.",
+	 		"c",
+	 
+	 		"Subhas and Sisir maintained a total secrecy",
+	 		"about the plan of escape.",
+	 		"about the plan of the picnic.",
+	 		"about the plan of travel.",
+	 		"a",
+	 
+	 		"Subhas wore a .",
+	 		"a Ray-Ban.",
+	 		"gold wire-rimmed spectacles.",
+	 		"Chopard Sunglasses.",
+	 		"b",
+	 
+	 		"The night was ",
+	 		"dark.",
+	 		"noisy.",
+	 		"moonlit.",
+	 		"c",
+	 
+	 		"The policemen sat on a ",
+	 		"bench.",
+	 		"charpoi.",
+	 		"staircase.",
+	 		"b",
+			
+			"Precints means ",
+	 		"a food.",
+	 		"boundary.",
+	 		"an insect.",
+	 		"b",  
+			
+			"The car engine faltered",
+	 		"many a time.",
+	 		"once.",
+	 		"more than twice.",
+	 		"b",
+	
+			"They arrived  near Dhanbad ",
+	 		"at 8.30pm.",
+	 		"at 8.30am.",
+	 		"at 5.30am.",
+	 		"b", 
+	
+			"Ziauddin left alone for",
+	 		"Gomoh station.",
+	 		"Howrah Station.",
+	 		"Sealdah Station.",
+	 		"a",
+	
+			"Ziauddin wanted to catch ",
+	 		"Rajdhani Express.",
+	 		"Shatabdi Express.",
+	 		"Delhi Kalka Mail.",
+	 		"c",
+	
+			"A sleepy porter",
+	 		"collected their luggage.",
+	 		"refused to carry their luggage.",
+	 		"was reluctant.",
+	 		"a", 
+	
+			"Subhas disappeared ",
+	 		"into the darkness.",
+	 		"into the narrow lane.",
+	 		"into his house.",
+	 		"a",
+	
+			"Sisir heard ",
+	 		"some soothing music.",
+	 		"the rhythmic clutter of wheels.",
+	 		"his favourite band music.",
+	 		"b"	
+    
+            
+        ];
