f5c11214d8f9c20ac82ad12e8e3b6ac51ee3ce76
Soumen Ch <38403158+soumench@users.noreply.github.com>
2021-09-14 00:04:12 +0530

    iuogkjg

--- e21/V/meminmarbl/engine.js
+++ e21/V/meminmarbl/engine.js
@@ -1,9 +1,13 @@
 
 
-var topicName = "~ Memory in Marble ~";
-document.getElementById("pageTitle").innerHTML = topicName;
+const topicName = "~ Memory in Marble ~";
+e("pageTitle").innerHTML = topicName;
 
-var path = window.location.pathname;
-var page = path.split("/").pop();
+const path = window.location.pathname;
+const page = path.split("/").pop();
 var thisPage = page.slice(3,-5); 
-document.getElementById("pageNum").innerHTML = "[  Page: " + thisPage + "  ]";
+e("pageNum").innerHTML = "[  Page: " + thisPage + "  ]";
+
+function e(id) {return document.getElementById(id);
+}
+/*Programmed by Soumen Chakraborty*/
