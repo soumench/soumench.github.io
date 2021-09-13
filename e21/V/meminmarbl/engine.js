9040868f21b1d8e15c573b8a86e09e4383723d77
Soumen Ch <38403158+soumench@users.noreply.github.com>
2021-09-13 20:40:49 +0530

    uouoiuoiiu

--- e21/V/meminmarbl/engine.js
+++ e21/V/meminmarbl/engine.js
@@ -1,3 +1,9 @@
 
 
 var topicName = "~ Memory in Marble ~";
+document.getElementById("pageTitle").innerHTML = topicName;
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var thisPage = page.slice(3,-5); 
+document.getElementById("pageNum").innerHTML = "[  Page: " + thisPage + "  ]";
