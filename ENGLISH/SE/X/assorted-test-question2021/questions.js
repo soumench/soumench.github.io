f23d9937e49c4344bce98912288de1d1bf8969b2
Soumen Ch <38403158+soumench@users.noreply.github.com>
2021-12-10 22:14:07 +0530

    wrywrwhshs

---
+++ ENGLISH/SE/X/assorted-test-question2021/questions.js
@@ -0,0 +1,94 @@
+const qa = [
+             1, "We cannot <span><u>understand</u><span/> what you are saying.(Supply a phrasal verb for the underlined word)","make out",
+    
+             2,"The quarrel was between.. ", "the mountain and the squirrel",
+    
+             3,"Swami's father wrote the letter to Swami's.... ", "Headmaster", 
+    
+             4,"I knew him.<br>(change the voice)", "He was known to me.",
+    
+             5,"The teacher said, 'Why is everybody late today?' (Change into Indirect Speech)", "no hint",
+    
+             6,"His life inspired us. <br>(Change the voice.)","We were inspired by his life.",
+    
+             7, "Swami's father asked him to ....", "deliver the letter to the Headmaster",
+    
+             8,"What a lovely sight! <br>( Change into an assertive sentence) ", "It is a very lovely sight.",
+        
+             9,"The teacher said to the students, \"The earth moves round the sun.\" <br>(Change into Indirect Speech.)", "The teacher told the students that the earth moves round the sun.",
+        
+            
+            10,"My cat ate a rat. <br>She went away.<br> (Use nominative absolute.)", "Having eaten a rat, my cat went away.",
+    
+    
+			11,"The old man in our neighbourhood is hard-working. <br> He is honest.<br>(Use a cumulative conjunction.)", "The old man in our neighbourhood is both hard-working and honest.",
+    
+    
+			12,"I still have a little money. <br> I can buy some food for the day.<br>(Use infinitive.) ","I still have a little money to buy some food for the day.",
+    
+    
+			13,"I go to school on foot.<br>(Rewrite using \"how\".)", "How do you go to school?",
+    
+            14,"One of the meanings of 'restore' is ...","repair",
+            15, "Beneficiary also means..", "recipients",
+			
+            16,"Swami felt dizzy because ...", " he was puzzled. ",
+    
+            17,"Other teachers of Swami were not as... "," friendly as he is.",
+    
+            18, "Who was the 'little prig'?","the squirrel",
+    
+            19,"The mountain is "," very big",
+    
+        
+			20, "Strangely, my little kitten is braver than my big dog.<br>(Use positive degree.)","Strangely, my big dog is not so brave as my little kitten.",
+    
+        
+			    
+			21,"The song started.<br> The baby danced gleefully.<br>(Join into a single sentence.)", "The song having started, the baby danced gleefully.",
+    
+    
+			22,"Soumen may go.<br> Soumen may stay. <br>(Join into a single sentence.)","Soumen may either go or stay.",
+    
+    
+			23,"Your teacher says something. <br>Listen to it minutely.<br> (Join into a single sentence.)", "Listen minutely to what your teacher says.",
+    
+			24,"A barking dog seldom bites.(Join using an adjective clause.)","A dog that barks seldom bites.",
+    
+    
+			25,"I definitely don't want an excuse.<br> (change the voice.)","Definitely an excuse is not wanted by me.",
+    
+    
+			26,"Please, speak in English.<br> (Change the voice.)", "You are requested to speak in English.",
+    
+            27,"Never ever disregard your parents.<br> (Change the voice.)", "Let your parents be never ever disregarded.",
+    
+           
+            28,"The more Swami thought of Samuel the more ...","aggrieved he felt.",
+            
+            29,"While going to school Swami felt ...","that he is the worst boy on the earth.",
+    
+            30,"Garbage causes pollution. It may be used as a source of energy. (Join into a single sentence using a relative clause) " ,"Garbage which may be used as a source of energy, causes pollution.",
+    
+            31,"Mention two qualities of the mountain."," no hint",
+        
+            32, "They <span><u>started</u><span/> their journey early in the morning.(Supply a phrasal verb for the underlined word)","set out",
+    
+            33,"Why did Swami’s conscience bother him?", " No hints. ",
+    
+            34, "Describe Samuel's appearance."," No hints",
+    
+            35, "The professor delivered a lecture to a group of students. (Change the Voice) ", "no hints",
+    
+            36, "The man <span><u>abandoned</u><span/> evil company .(Supply a phrasal verb for the underlined word)","gave up",
+            
+            37, "Synonym of 'inaugural'..","opening",
+            
+            38, "Nearest in meaning of'specialists'","experts",
+    
+                
+            39, "Some topics to study for Composition / writing..","1.Omlette preparation process<br>2. tea preparation process<br>3.Notice to your schoolmates for the forthcoming educational tour to Murshidabad <br> 4. notice to arrange a visit to  a place of historial interest<br>5.Editorial letter on maintaining covid protocol  <br> 6.Editorial letter on spreading of covid19<br>"
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018-2021*/
