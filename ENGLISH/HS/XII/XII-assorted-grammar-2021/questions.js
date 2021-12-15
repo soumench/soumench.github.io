ef81fad39ca6b2a762dedf91ae1da854a462792b
Soumen Ch <38403158+soumench@users.noreply.github.com>
2021-12-15 23:13:17 +0530

    tejsjsfs

--- ENGLISH/HS/XII/XII-assorted-grammar-2021/questions.js
+++ ENGLISH/HS/XII/XII-assorted-grammar-2021/questions.js
@@ -5,21 +5,21 @@ const qa = [
     
             3,"\"Where are you going?\", she asked.<br>(Change the mode of narration) ", "She asked me where I was going.",
         
-            4,"She would forget our brief encounter. <br> ( Change the voice.)", "Our brief encounter would be forgotten by her. <br> ( >>>>  important  <<<< ) ",
+            4,"She would forget our brief encounter. <br> ( Change the voice.)", "Our brief encounter would be forgotten by her.",
     
-            5,"How does the Poet bring out the futility of war in the poem 'Asleep in the valley'?", " no hint <br> (>>>  important <<<)",
+            5,"How does the Poet bring out the futility of war in the poem 'Asleep in the valley'?", " no hint",
         
             6,"It takes much time to kill a tree.<br>(Change the voice.)", "Much time is taken to kill a tree.",
         
             7,"All the necessities were provided for.<br>They were in terms of food, medicine and clothing.<br>(Join.)", "All the necessities in terms of food, medicine and clothing were provided for.",
     
-            8, "A man, getting into the compartment, stammered an apology. <br> ( Split into two simple sentences.)", "A man was getting into an apartment.<br> He stammered an apology. <br>( >>>> important <<<<)",
+            8, "A man, getting into the compartment, stammered an apology. <br> ( Split into two simple sentences.)", "A man was getting into an apartment.<br> He stammered an apology.",
     
-    		9, "This is not a correct approach. <br> (to Affirmative Sentence)","This is an incorrect approach.  <br> ( >>> important <<<<)",
+    		9, "This is not a correct approach. <br> (to Affirmative Sentence)","This is an incorrect approach.",
     
 			10,"A soldier, very young, lies open-mouthed.<br> (Rewrite as a complex sentence.)", "A soldier who is very young, lies open-mouthed.",
     
-            11,"She was silent. ( to Negative Sentence.)","She was not speaking. <br> (>>> important <<<)",
+            11,"She was silent. ( to Negative Sentence.)","She was not speaking.",
     
 			12, "The man who had entered the compartment broke into my reverie.<br> (Split into two simple sentences.)","The man entered the compartment. He broke into my reverie.",
     
@@ -30,19 +30,19 @@ const qa = [
     
 			15,"I am neither a poet <u>or</u> a novelist.<br>(Correct the error.)","I am neither a poet nor a novelist",
     
-            16, "The train gathered speed and the wheels took up their song. <br> (Turn into a Simple Sentence.)", "................... <br> ( >>>  important   <<<<< )",
+            16, "The train gathered speed and the wheels took up their song. <br> (Turn into a Simple Sentence.)", "................... ",
     			
             17,"Few girls can <u>resists</u> flattery.<br> (Correct the error.)", "Few girls can resist flattery.",
     
             18, "The poet says, 'They fill the hollow full of light'. <br> ( Turn into an indirect speech.)","The poet says that they filled the hollow full of light.",
     
-            19, "Name the three characters of the play 'The Proposal'."," no hint <br> ( >>> important <<<)",
+            19, "Name the three characters of the play 'The Proposal'."," no hint",
     
-            20, "Whom did Lomov inherit the meadows from? ", " no hint <br>  (>>> important <<<)",
+            20, "Whom did Lomov inherit the meadows from? ", " no hint",
     
-            21, "What will make the beauty of the poet's friend eternal?", " no hint  ( >>> important   <<< )",
+            21, "What will make the beauty of the poet's friend eternal?", " no hint ",
     
-            22, "Who would receive the girl at Saharanpur station?", " no hint  <br>  ( >>> important  <<<)",
+            22, "Who would receive the girl at Saharanpur station?", " no hint ",
     
             23,"They are ours. Now everybody knows this. <br>( join )", "Now everybody knows that they are ours.",
     
@@ -50,7 +50,7 @@ const qa = [
                
             25,"I don’t want the meadows.<br> ( Change the voice. ) ", "The meadows are not wanted by me.",
             
-            26, "How is the soldier lying in the sunlight? ", "no hint <br> ( >>> important  <<<< )",
+            26, "How is the soldier lying in the sunlight? ", "no hint ",
     
             27, "All the necessities were provided for.<br> They were in terms of food, medicine and clothing.<br> (Join into a single sentence.)","All the necessities in terms of food, medicine and clothing were provided for.",
             
@@ -62,13 +62,13 @@ const qa = [
     
             31, "Who is apparently lying asleep in the valley? ", "The dead soldier",
             
-            32, "Kalam's father would take him to the Mosque for ..", "evening prayers  <br>  ( >>> important  <<<)",
+            32, "Kalam's father would take him to the Mosque for ..", "evening prayers ",
     
-            33, "Lomov comes to Chubukov's house ....", " to propose his daughter <br>  ( >>> important <<<)",
+            33, "Lomov comes to Chubukov's house ....", " to propose his daughter ",
     
-            34, "'Oh how lucky you are' - Who is the speaker here?<br> Who is ‘you’ referred to here?<br> Why is the speaker spoken to considered lucky?", " no hint  <br> (>>> important <<<)",
+            34, "'Oh how lucky you are' - Who is the speaker here?<br> Who is ‘you’ referred to here?<br> Why is the speaker spoken to considered lucky?", " no hint ",
     
-            35, "'I have come to you to trouble you with a request' - Who is 'I' and 'you' referred to in the sentence?", "no hint <br> ( >>> important <<< )",
+            35, "'I have come to you to trouble you with a request' - Who is 'I' and 'you' referred to in the sentence?", "no hint ",
             
             36, "Important topics on composition:", "1. Report on educational tour <br> 2. Application to the bank manager to issue a new passbook <br> 3. Report on the Covid protocol in classroom <br>4. Application to the Bank manager informing the change of residence "
         ];
