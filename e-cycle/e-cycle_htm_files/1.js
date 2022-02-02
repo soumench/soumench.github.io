eb0adb27d7645932cc2fbde83e3300208664f876
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-02-03 00:39:38 +0530

    ecl

---
+++ e-cycle/e-cycle_htm_files/1.js
@@ -0,0 +1,42 @@
+window.local_imageGallerySimple_v11_2_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "e-cycle_htm_files/291.jpg"
+},
+{
+ image: "e-cycle_htm_files/292.jpg"
+},
+{
+ image: "e-cycle_htm_files/293.jpg"
+},
+{
+ image: "e-cycle_htm_files/294.jpg"
+},
+{
+ image: "e-cycle_htm_files/295.jpg"
+},
+{
+ image: "e-cycle_htm_files/296.jpg"
+},
+{
+ image: "e-cycle_htm_files/297.jpg"
+},
+{
+ image: "e-cycle_htm_files/298.jpg"
+},
+{
+ image: "e-cycle_htm_files/299.jpg"
+},
+{
+ image: "e-cycle_htm_files/300.jpg"
+}
+];
