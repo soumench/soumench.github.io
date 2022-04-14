7b072873b7544f4728bc24ae56bcda5cde68cdbe
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-04-15 00:56:09 +0530

    reue7m,urykrkk

---
+++ e22/viii_eng/wndcp_mcq/qsource/q.js
@@ -0,0 +1,124 @@
+const topic_name =" M.C.Q. on \"The Wind Cap\""; 
+const list=[
+            "The lad wanted to become",
+	 		"a soldier.",
+	 		"a sailor.",
+	 		"an actor.",
+	 		"a",
+	 
+	 
+            "jon was a",
+	 		"an old man.",
+	 		"a man.",
+	 		"boy.",
+	 		"c",
+	
+	        "Jon was the son of a",
+	 		"teacher",
+	 		"farmer.",
+	 		"priest.",
+	 		"b",
+	 		
+	 		"Jon always obeyed his",
+	 		"mother.",
+	 		"father.",
+	 		"uncle.",
+	 		"a",
+	 		
+	 		"Jon was ",
+	 		"Kind hearted.",
+	 		"cruel.",
+	 		"stoic.",
+	 		"a",
+	 		
+	 		"Jon obeyed his mom but",
+	 		"he was sorry.",
+	 		"he was cheerful.",
+	 		"he was curious.",
+	 		"a",
+	 		
+	 		"Jon used to work in a ",
+	 		"shop.",
+	 		"farm.",
+	 		"garrage.",
+	 		"b",
+	 
+	 		"Jon ran over a ",
+	 		"tiny green rat.",
+	 		"tiny green cat.",
+	 		"tiny green turtle.",
+	 		"c",
+	 
+	 		"Jon placed the turtle on his",
+	 		"head.",
+	 		"shoulder.",
+	 		"on a bolder.",
+	 		"a",
+	 
+	 		"The tiny fairy ",
+	 		"scolded Jon.",
+	 		"thanked Jon.",
+	 		"hit Jon.",
+	 		"b",
+	 
+	 		"The green fairy man could",
+	 		"read a newspaper.",
+	 		"read a will.",
+	 		"read a heart.",
+	 		"c",
+	 
+	 		"Jon wished to go",
+	 		"sailing.",
+	 		"shopping.",
+	 		"to school.",
+	 		"a",
+	 
+	 		"The fairy gave Jon ",
+	 		"a magic carpet.",
+	 		"a magic cap.",
+	 		"a magic towel.",
+	 		"b",
+	 
+	 		"The magic cap was full of",
+	 		"gold.",
+	 		"sand.",
+	 		"wind.",
+	 		"c",
+	 
+	 		"Jon had a caring nature.",
+	 		"No.",
+	 		"Yes.",
+	 		"Both yes and no.",
+	 		"b",
+			
+			"A ship was anchored ",
+	 		"near the ocean.",
+	 		"near the shore.",
+	 		"near the mountain.",
+	 		"b",
+            
+            "Jon became popular as he",
+	 		"could supply the petrol.",
+	 		"could supply the wind.",
+	 		"could supply the diesel.",
+	 		"b",
+    
+            "Jon did not set his foot on land",
+	 		"for a month.",
+	 		"for a week.",
+	 		"for a year and a day.",
+	 		"c",
+    
+            "Jon owned his own",
+	 		"boat and a hundred acre farm.",
+	 		"large ship and some land.",
+	 		"shipyard.",
+	 		"a",
+    
+            "Finally Jon spent half the year",
+	 		"on a ship and rest on the shore.",
+	 		"with the captain.",
+	 		"with the fairy.",
+	 		"b"
+			
+        ];
