0bbb796fb6564d004612403b7f08da5701f6ac2f
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-08-14 22:08:26 +0530

    dhsdhsh

---
+++ ENGLISH/SE/VIII/Sum2_Eng_2022_suggestions/questions.js
@@ -0,0 +1,47 @@
+const qa = [ 
+            1,"Who was weeping alone in the palace?", "Princess September.", 
+    
+            2,"Who hopped in and began to sing?", "A little song bird.",
+    
+            3,"Who was the father of princess September? ", "King of Siam.",
+        
+            4,"What served as a perch of the little bird?", "The first finger of the right hand of princess September",
+    
+            5,"Why did Princess September clap her hands? ", " no hint",
+        
+            6,"Who was Uther-Pendragon?", "no hint.",
+        
+            7,"Who was Lady Igraine?", "no hint",
+    
+            8, "Who was sir Ector?", "no hint",
+    
+    		9, "Who was Merlin? "," no hint",
+    
+			10,"Who was Sir Ulfius? ", "No hint",
+    
+            11,"Who is compared to a hungry dog? ","The Sea",
+    
+			12, "Who remains quiet in the middle of the year?","The Sea",
+    
+        
+			13,"Barun came back home when it was raining.(Find the group of words that do the work of an Adverb)", " when it was raining",
+    			  
+			14,"I didn't get it as I was not present there.(Find the group of words that do the work of an Adverb)", "as I was not present there.",
+    
+			15,"Add prefix to the word > happy","un..",
+    
+            16, "Add suffix to the word > cloud", "less ",
+    			
+            17,"Make meaningful sentences with the words > [we, brave, country, message, movement, freedom, money, joy, hope, playful, sweet, respected, famous, notorious]", "no hint",
+    
+            18, "Supply the antonyms of the following words: [ closed, kind, send, take, love, joy, meaningful, confident, steady, despair]","no hint",
+    
+            19, "Write a letter to your younger brother advising him to be kind to animals."," no hint",
+    
+            20, "Write a letter to your younger sister advising her to restrict the use of mobile phone.", " no hint"
+    
+            
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018-22*/
