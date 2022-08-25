456c97589d1ead0b27638d460055106ceb45f2fa
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-08-25 20:16:21 +0530

    rujfdsjDietikkzgkg

---
+++ ENGLISH/SE/VI/Sum2_Eng_2022_suggestions/questions.js
@@ -0,0 +1,46 @@
+const qa = [ 
+            1,"Make meaningful sentences with the following words: news, important, steady, show, picture, gentleman, freedom, duty, love, school, hobby, ", "no hint", 
+    
+            2,"Add a prefix or a suffix to the following words to form opposites: happy, learn, cloud, stain, understand, correct, appear, use, agree, connect, like ", "no hint",
+    
+            3,"Tick  the appropriate form of the verbs given in brackets: Do you not (hear/ heard) the bell?", "hear",
+        
+            4,"Tick  the appropriate form of the verbs given in brackets: No news (is / are) good news", "is",
+    
+            5,"Imagine you met a fairy in your dreams. Write in about sixty words what gift would you have asked from her and why.  ", " no hint",
+        
+            6,"Fill in the blanks with suitable articles. I saw ____ magic show. ", "a/the",
+        
+            7,"Fill in the blanks with suitable articles. Brutus is ____ honourable man.. ", "an",
+    
+            8, "Fill in the blanks with suitable articles. Luck is __ idol of the idle.", "the",
+    
+    		9, "Fill in the blanks with suitable articles. __ father is a banker provided by nature.","A",
+    
+			10,"Whom did the kites get as companions high up in the quite air?	", "No hint",
+    
+            11,"How did the little kite prepare himself for flight?","no hint",
+    
+			12, "What did the big kite tell the little kite to do?	","no hint",
+    
+        
+			13,"Who dragged the author up to the magic shop?", "no hint",
+    			  
+			14,"What was the price of the glass ball?", "no hint",
+    
+			15,"Add prefix to the word > happy","un..",
+    
+            16, "Add suffix to the word > cloud", "less ",
+    			
+            17,"Who had a queen and several children?", "no hint",
+    
+            18, "Supply the antonyms of the following words: [ closed, kind, send, take, love, joy, meaningful, confident, steady, despair]","no hint",
+    
+            19, "How to be kind to animals."," no hint"
+    
+                
+            
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018-22*/
