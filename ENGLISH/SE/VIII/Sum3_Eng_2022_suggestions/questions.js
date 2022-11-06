5fc098ddf6be4968d5ce4c0537ee37b217f1fb1c
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-11-06 22:12:46 +0530

    ldkghladpoetnssfsf

---
+++ ENGLISH/SE/VIII/Sum3_Eng_2022_suggestions/questions.js
@@ -0,0 +1,47 @@
+const qa = [ 
+            1,"Which mountain range is mentioned in the text 'The Man Who Planted Trees'", "Alps.", 
+    
+            2,"What was the month when the author went on a long hike in the text 'Man Who Planted Trees'?  ", "June",
+    
+            3,"Whose sound was compared to the sound of a wild beast disturbed during its meal in the text 'The Man Who Planted Trees'?", "The whistling sound of the wind high up in the sky.",
+        
+            4,"Is swallow a summer bird?", "yes",
+    
+            5,"Why does the swallow leave the poet during the winter?", " no hint",
+        
+            6,"What does Swallow do in Summer?", "no hint.",
+        
+            7,"When does the sparrow come to the poet?", "no hint",
+    
+            8, "She drew a picture.(Change the voice)", "A picture was drawn by her.",
+    
+    		9, "Cricket is played by them.","They play cricket.",
+    
+			10,"We must endure what we cannot cure.", "What cannot be cured must be endured.",
+    
+            11,"The book (contain / contained ) a story.( Pickup the correct verb form) ","contain",
+    
+			12, "The people of England ( was / were ) unhappy.( Pickup the correct verb form) ","were",
+    
+        
+			13,"Father said, 'Switch off the TV'", "no hint",
+    			  
+			14,"He said, 'I have finished my homework'", "no hint",
+    
+			15,"Add prefix to the word > possible","im..",
+    
+            16, "Add suffix to the word > spot", "less ",
+    			
+            17,"Make meaningful sentences with the words > [here, clever, village, high, good, bondage, poor,ecstasy , hopeless, thoughtless, bitter, disrespect, infamous, content]", "no hint",
+    
+            18, "Supply the antonyms of the following words: [unkind, receive, give , hate, sorrow, meaningless, nervous, wobbly, hope]","no hint",
+    
+            19, "Suppose you have to obey the orders of someone from morning to night. You are not allowed to act according to your own will. Write a paragraph in about eighty words describing feelings in this situation.  "," no hint",
+    
+            20, "Write an imaginary conversation between you and a tree which you had planted and which has now grown green and tall.", " no hint"
+    
+            
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018-22*/
