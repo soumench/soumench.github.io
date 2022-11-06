1eb7930778ca66a033e7a58dde14adcec77bb78a
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-11-06 23:01:35 +0530

    juERTANaranerwyeeue

---
+++ ENGLISH/SE/IX/assorted-questions-third-summative-ix/js_engine/q.js
@@ -0,0 +1,63 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Assorted Sample Questions <br>Class-IX";
+var bank = [
+    "Where did the shipwrecked sailor sleep at night?~no hint."
+    
+    , "Who was Robinson Crusoe?  ~ no hint"
+    
+    , "What happened to the rest of the Crusoe's ship’s company? ~ no hint"
+    
+    , "What were the things that Robinson collected from the wreck of his ship?~no hint"
+    
+    ,"What did the wind do to the second ship?~no hint"
+    
+    ,"Which ships came back happily or unhappily?~no hint"
+    
+    ,"Write the describing words that the poet used to describe the different seas.~no hint"
+    
+    ,"Man is mortal.<br>(to Negative Sentence)~No man is immortal."
+    
+    ,"Nikola Tesla was a great inventor.<br>(to Interrogative sentence)~Was not Nikola Tesla a great inventor?"
+    
+    ,"I lost my mobile but I have found it. (Change into a complex sentence.)~ no hint"
+    
+    ,"The sunset is very beautiful.<br>(to Exclamatory)~What a beautiful sunset!"
+    
+    ,"Everest is the highest peak in the world.<br>(to Positive degree)~No other peak in the world is as high as Everest."
+    
+    ,"We all take pride in our India.<br>(Use adjective form of 'pride'.)~We all are proud of our India."
+    
+    ,"You have kept the rule, but broken its spirit.<br>(to Complex)~Though you have kept the rule, you have broken its spirit."
+    
+    ,"It was a bitterly cold night.<br>(to complex)~It was a night which was bitterly cold."
+    
+    ,"Am I not a very fine bird?<br>(to Assertive)~I am a very fine bird."
+    
+    ,"He became excited.<br>(Rewrite using noun form of 'excited')~He was filled with excitement."
+    
+    ,"He uttered a joyful scream.<br>(Use adverb form of 'joyful'.)~He screamed joyfully."
+    
+    ,"I had neither food nor clothes.<br>(to affirmative )~ I was without food and clothes."
+    
+    ,"He is a happy man.<br>(to complex)~ He is a man who is happy."
+    
+    ,"I cannot but weep.<br>(Omit 'but'.)~I cannot help weeping."
+    
+    ,"Everybody loves his motherland.<br>(to interrogative)~Who does not love his motherland?"
+    
+    ,"Where is the newspaper? “, he asked. (Change the mode of narration.)~no hint"
+    
+    ,"We must eat to live. (Change into a compound sentence.)~ no hint"
+    
+    ,"He said to me, “I have done it.” (Change the mode of narration.)~ no hint"
+    
+    ,"You have been ill for three days. Write a letter to the Headmaster of your school seeking leave of absence for those days.~ no hint"
+    
+    ,"Use the following flow-chart to write a paragraph within 100 words on how to make paper.~ cutting down of bamboos --- making pulp --- adding chemicals --- rolling into sheets --- adding colour ---drying --- ready for use"
+
+            
+];
