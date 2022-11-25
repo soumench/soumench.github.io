fcc7a0a29836ad1f6c99a0c6dbd39cbd2b189296
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-11-26 00:12:35 +0530

    ddhfsryrhhe

---
+++ ENGLISH/HS/XI/XI-HY-ENG-22/js_engine/q.js
@@ -0,0 +1,128 @@
+/*
+Copyright: Soumen Chakraborty 2018
+https://soumen-here.blogspot.com/
+Strictly for Educational and Non-commercial Purpose
+*/
+heading.innerHTML="Assorted Sample Questions for The Half Yearly Exam<br>Class-XI";
+var bank = [
+    
+    "Who had pardoned Jimmy?~no hint"
+    
+    ,"What did Jimmy enjoy in the restaurant? ~the sweet joy of liberty in the form of a broiled chicken, a bottle of white wine and a cigar of superior quality."
+    
+    ,"'That child- she cannot stand it long in there.'- Who is referred to here?~ Agatha, the younger daughter of Annabels sister"
+    
+    ,"What did Jimmy want to give away to his old friend Billy? ~ his set of burglers' tools"
+    
+    ,"Who was Ben Price? ~ the famous detective"
+    
+    ,"Who was Ralph D Spencer? ~ alias of Jimmy Valentine himself"
+    
+    ,"Who was Mr. Adams? ~ the owner of Elmore Bank and the father of Annabel"
+    
+    ,"What is Phoenix? ~ an immortal bird associated with Greek mythology"
+    
+    //--
+    
+    ,"Who asked Sidda to throw the ball into the sky?~Leela"
+    
+    ,"What did Leela ask Sidda to draw?~two and three letters of alphabet and a kind of cat and crow"
+    
+    ,"In which Hotel did Jimmy engage a room in Elmore?~ the Planter's Hotel"
+    
+    ,"What was Mr. Sivasankar brooding over, standing in the verandah of his house? ~no hint"
+    
+    ,"'Dear God!'- What feeling does the expression convey?~No hint."
+    
+    ,"How did Leela try to make Sidda write? ~ no hint"
+    
+    ,"'Sidda knows the moon.' Who said this? ~ Leela said this to her mother."
+    
+    ," Who first noticed that gold chain was missing?~ Leela's mother"
+    
+    
+    
+   
+    //---poem--start-----------------------------//
+    
+   
+    
+    ,"How does Wordswoth describe London in his poem?~ no hint"
+    
+    ,"How does Wordsworth describe the air in the city?~smokeless"
+    
+    ,"How does the river Thames flow in the poem 'Upon Westminster Bridge'? ~ 'in his own sweet will'"
+    
+    ,"What is the 'first splendour' as mentioned in the poem 'Upon Westminster Bridge'? ~ the rising sun"
+    
+    ,"What was the time when Wordsworth viewed London? ~ in the early morning"
+    
+    //--
+    
+    ,"Justify the title of the poem 'Meeting at Night'.~ no hint"
+    
+    , "What is the colour of the sea in 'Meeting at NIght'? ~ grey"
+    
+    ,"How many fields will the poet have to cross before a firm appears? ~'three fields to cross till a firm appears'"
+    
+    ,"How does Browning describes the Half Moon?  ~ yellow, large and low"
+        
+    , "What garment did the city wear?~The beauty of the morning."
+    
+    ,"Where does the lady-love (mentioned in the poem 'Meeting at Night') live? ~ in a farm house"
+    
+    ,"What is the colour of the moon in the poem 'Meeting at Night'? ~ yellow"
+    
+    //-------
+    
+     //--- play-----start------------------------//
+    
+    ,"What was the surprising prediction of the third witch?~no hint"
+    
+    ,"Why did Lady Macbeth commit suicide?~ no hint"
+    
+    ,"Who were the sons of King Duncan?~Malcolm( elder), Donalbain(younger) "
+    
+    ,"How did the witches look like?~ no hint"
+    
+    ,"Why could Lady Macbeth not murder King Duncan by herself?~ no hint"
+    
+    ,"Give a brief account of 'Banquo Scene' in Macbeth.~ no hint"
+    
+    ,"What did each of the three witches predict to Macbeth? ~ no hint"
+    //--- 
+        
+    //-----grammar---start----------------------//
+    ," Sir, do you want a servant? (Change the voice). ~ no hint"
+    
+    ,"'Tell me a story, mother', Leela said.(Change the mode of narration) ~ no hint"
+    
+    ,"'I don't know master,' Sidda said. (Change the mode of Narration) ~ Sidda told the master that he did not know."
+    
+    
+    ,"He got his keys and went upstairs. ( split into two simple sentences) ~ no hint"
+    
+    ,"And then Ben Price acted rather strangely.( Rewrite using adjective form of 'strangely'.)~ no hint"
+    
+    ,"'Guess you're mistaken,Mr. Spencer', he said. ( Change the mode of narration) ~ He told Mr. Spencer that he guessed that he were mistaken."
+    
+    
+    ,"He stopped near the well and pointed down.(Change into a simple sentence.)~ no hint"
+    
+    ,"All the mighty heart is lying still.(Change into a complex sentence. ) ~ no hint"
+    
+    ,"The very houses seem asleep.(Use the adverb form of 'seem')~ THe very houses are seemingly asleep."
+    //-----
+    
+    
+    
+    //---composition//
+    
+    ,"Write a paragraph in about 200 words on the Uses and abuses of cellphones~ no hint"
+    
+    ,"Write a paragraph in about 200 words on the importance of maintaining covid protocol. ~ no hint"
+    
+    ,"Write a paragraph in about 200 words on the pros and cons of online classes. ~ no hint"
+
+            
+];
