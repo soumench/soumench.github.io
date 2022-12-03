0003c6dd9f6e540d05f1f7e2a72e6637b74dc7b2
Soumen Ch <38403158+soumench@users.noreply.github.com>
2022-12-03 21:28:48 +0530

    rujeddekaryasrylrkdg

---
+++ ENGLISH/spknEng/questions.js
@@ -0,0 +1,11 @@
+const qa = [ 
+ 1.,"Are you ready?","তুমি কি প্রস্তুত?" "areUready_snd.mp3",
+ 2.,"What do you want?", "তুমি কি চাও?", "whatDouwant_snd.mp3",
+ 3., "Where are you now?", "তুমি কোথায় এখন?তুই কোথায় আছিস?", "whereRunow_snd.mp3"        
+    
+    
+    
+        ];
+        const length = qa.length;
+
+/*Copyright- Soumen Chakraborty 2018-22*/
